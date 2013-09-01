9e028179a0ceb2ac620a3138c859a7c8f45d5642
Light2 <light_pswg@yahoo.com>
2013-09-01 15:20:37 +0200

    Fix stamina skill mod

--- scripts/skillMods/stamina_modified.py
+++ scripts/skillMods/stamina_modified.py
@@ -2,13 +2,13 @@ import sys
 
 def add(core, actor, name, base):
 	actor.addSkillMod(name, base)
-	actor.setMaxHealth(actor.getHealth() + base * 2)
-	actor.setMaxAction(actor.getAction() + base * 8)
+	actor.setMaxHealth(actor.getMaxHealth() + base * 2)
+	actor.setMaxAction(actor.getMaxAction() + base * 8)
 	return
 	
 def deduct(core, actor, name, base):
 	actor.deductSkillMod(name, base)
-	actor.setMaxHealth(actor.getHealth() - base * 2)
-	actor.setMaxAction(actor.getAction() - base * 8)
+	actor.setMaxHealth(actor.getMaxHealth() - base * 2)
+	actor.setMaxAction(actor.getMaxAction() - base * 8)
 	return
 	
