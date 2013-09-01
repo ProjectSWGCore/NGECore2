f6d56abf7a28ca1fb091e79bb0a928ef1d550981
Light2 <light_pswg@yahoo.com>
2013-09-01 15:19:24 +0200

    Fixed constitution skill mod

--- scripts/skillMods/constitution_modified.py
+++ scripts/skillMods/constitution_modified.py
@@ -2,13 +2,13 @@ import sys
 
 def add(core, actor, name, base):
 	actor.addSkillMod(name, base)
-	actor.setMaxHealth(actor.getHealth() + base * 8)
-	actor.setMaxAction(actor.getAction() + base * 2)
+	actor.setMaxHealth(actor.MaxHealth() + base * 8)
+	actor.setMaxAction(actor.getMaxAction() + base * 2)
 	return
 	
 def deduct(core, actor, name, base):
 	actor.deductSkillMod(name, base)
-	actor.setMaxHealth(actor.getHealth() - base * 8)
-	actor.setMaxAction(actor.getAction() - base * 2)
+	actor.setMaxHealth(actor.getMaxHealth() - base * 8)
+	actor.setMaxAction(actor.MaxAction() - base * 2)
 	return
 	
