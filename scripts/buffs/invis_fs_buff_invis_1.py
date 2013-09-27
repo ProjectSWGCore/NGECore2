f2c2dbc537c60422279308ff9f53a369249cfb38
Doogla <masterdoogla@yahoo.com>
2013-09-27 04:37:19 -0700

    removed useless skill mods

--- scripts/buffs/invis_fs_buff_invis_1.py
+++ scripts/buffs/invis_fs_buff_invis_1.py
@@ -1,13 +1,8 @@
 import sys
 
 def setup(core, actor, buff):
-	core.skillModService.addSkillMod(actor, 'movement', 0)
-	core.skillModService.addSkillMod(actor, 'invis', 0)
-	
 	return
 	
 def removeBuff(core, actor, buff):
-	core.skillModService.deductSkillMod(actor, 'movement', 0)
-	core.skillModService.deductSkillMod(actor, 'invis', 0)
 	return
 	
