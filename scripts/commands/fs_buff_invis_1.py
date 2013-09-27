9331ec2f64a4ca031a0ac9f706bf6c43bd6a152a
Doogla <masterdoogla@yahoo.com>
2013-09-27 03:38:27 -0700

    a few jedi abilities
    added force run with particle effects, saber intercept, saber block,
    added particle effects for stance and focus, moved the marker for Dee'ja
    Peak to its correct location on the map.

---
+++ scripts/commands/fs_buff_invis_1.py
@@ -0,0 +1,9 @@
+import sys
+
+def setup(core, actor, buff):
+	return
+	
+def run(core, actor, target, commandString):
+	core.buffService.addBuffToCreature(actor, 'invis_fs_buff_invis_1')
+	return
+	
---
+++ scripts/commands/fs_saber_reflect_buff.py
@@ -0,0 +1,9 @@
+import sys
+
+def setup(core, actor, buff):
+	return
+	
+def run(core, actor, target, commandString):
+	core.buffService.addBuffToCreature(actor, 'fs_saber_reflect')
+	return
+	
