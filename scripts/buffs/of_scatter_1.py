b727f229839b7870cfa8fda9fb77ce2eb8e74505
xbang21 <austinbangbang@yahoo.com>
2013-11-06 17:40:59 -0500

    fixed tiny bug

--- scripts/buffs/of_scatter_1.py
+++ scripts/buffs/of_scatter_1.py
@@ -8,8 +8,8 @@ def setup(core, actor, buff):
 	return
 	
 def removeBuff(core, actor, buff):
+	core.skillModService.deductSkillMod(actor, 'movement', 5)
 	core.skillModService.deductSkillMod(actor, 'expertise_glancing_blow_all', 2)
 	core.skillModService.deductSkillMod(actor, 'movement_resist_snare', 15)
 	core.skillModService.deductSkillMod(actor, 'movement_resist_root', 2)
-	core.skillModService.deductSkillMod(actor, 'movement', 5)
 	return
