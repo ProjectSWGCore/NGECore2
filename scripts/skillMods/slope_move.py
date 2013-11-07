2410853e965361012bca6114e685ea65a14d86da
Doogla <masterdoogla@yahoo.com>
2013-11-07 10:47:43 -0800

    Buffs Take 3
    Moved the speed modifier of force run to its correct skill mod script
    instead of being in the buff itself

--- scripts/skillMods/slope_move.py
+++ scripts/skillMods/slope_move.py
@@ -2,9 +2,11 @@ import sys
 
 def add(core, actor, name, base):
 	actor.addSkillMod(name, base)
+	actor.setSpeedMultiplierBase(2.5 + (2.5 * (actor.getSkillModBase('expertise_movement_buff_fs_force_run')) / 100))
 	return
 	
 def deduct(core, actor, name, base):
 	actor.deductSkillMod(name, base)
+	actor.setSpeedMultiplierBase(1)
 	return
 	
