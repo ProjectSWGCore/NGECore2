92fad898f5b0faa54c452fe3aeefcdbe0af7eae9
Doogla <masterdoogla@yahoo.com>
2013-11-07 11:25:34 -0800

    Buffs Take 4
    Going back to including the speed modifier in the buff so it doesn't
    make you the flash when leveling.

--- scripts/buffs/fs_force_run.py
+++ scripts/buffs/fs_force_run.py
@@ -4,7 +4,8 @@ def setup(core, actor, buff):
 	buff.setDuration(buff.getDuration()+(actor.getSkillModBase('expertise_buff_duration_line_fs_force_run')))
 	actor.playEffectObject('clienteffect/pl_force_run.cef', 'fs_force_run')
 	actor.playEffectObject('appearance/pt_force_speed.prt', 'fs_force_run')
-	core.skillModService.addSkillMod(actor, 'slope_move', 1)
+	actor.setSpeedMultiplierBase(2.5 + (2.5 * (actor.getSkillModBase('expertise_movement_buff_fs_force_run')) / 100))
+	core.skillModService.addSkillMod(actor, 'slope_move', 5)
 	core.skillModService.addSkillMod(actor, 'movement_resist_snare', 100)
 	core.skillModService.addSkillMod(actor, 'movement_resist_root', 100)
 	
@@ -15,7 +16,8 @@ def setup(core, actor, buff):
 	
 def removeBuff(core, actor, buff):
 	##Waiting for a remove particle effect function in CreatureObject. Twinkle toes won't go away without it.
-	core.skillModService.deductSkillMod(actor, 'slope_move', 1)
+	actor.setSpeedMultiplierBase(1)
+	core.skillModService.deductSkillMod(actor, 'slope_move', 5)
 	core.skillModService.deductSkillMod(actor, 'movement_resist_snare', 100)
 	core.skillModService.deductSkillMod(actor, 'movement_resist_root', 100)
 	return
