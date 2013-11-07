dd231117c84fad69ab10e4598f3f66c6e928cdbe
Doogla <masterdoogla@yahoo.com>
2013-11-07 09:30:03 -0800

    Jedi Buff Update (Take 2)
    No project file this time.
    
    Force Run speed and duration based on expertise (I promise this does
    work.)
    
    Saber Block parry affected by expertise

--- scripts/buffs/saberblock.py
+++ scripts/buffs/saberblock.py
@@ -1,11 +1,10 @@
 import sys
 
 def setup(core, actor, buff):
-	core.skillModService.addSkillMod(actor, 'expertise_saber_block', 10)
-	
+	core.skillModService.addSkillMod(actor, 'display_only_parry', 1000 + (actor.getSkillModBase('expertise_saber_block') * 100))
 	return
 	
 def removeBuff(core, actor, buff):
-	core.skillModService.deductSkillMod(actor, 'expertise_saber_block', 10)
+	core.skillModService.deductSkillMod(actor, 'display_only_parry', 1000 + (actor.getSkillModBase('expertise_saber_block') * 100))
 	return
 	
