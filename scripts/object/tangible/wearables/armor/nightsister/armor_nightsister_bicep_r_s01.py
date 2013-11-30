15827ae754fe363debef10e3a1aac969a33ee442
Skalex223 <johnmwagner2005@yahoo.com>
2013-11-29 20:01:43 -0500

    ns bracer
    forgot ns bracer

--- scripts/object/tangible/wearables/armor/nightsister/armor_nightsister_bicep_r_s01.py
+++ scripts/object/tangible/wearables/armor/nightsister/armor_nightsister_bicep_r_s01.py
@@ -4,7 +4,6 @@ def setup(core, object):
 
 	object = core.objectService.createObject('object/tangible/wearables/armor/nightsister/shared_armor_nightsister_bicep_r_s01.iff', object.getPlanet())
 	object.setStringAttribute('condition', '100/100')
-	object.setStringAttribute('volume', '1')
-	inventory.add(object)
+	object.setIntAttribute('volume', 1)
 	
 	return
