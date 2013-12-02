18f0c6cc740d4dc3191b6e583fc0c48bc6ce6620
Ziggeh <ziggy@projectswg.com>
2013-12-03 00:38:51 +0100

    Removed unreferenced variable

--- scripts/starter_clothing/crafting_artisan_trandoshan_male.py
+++ scripts/starter_clothing/crafting_artisan_trandoshan_male.py
@@ -5,7 +5,6 @@ def CraftingArtisanTrandoshanMale(core, object):
 	vest = core.objectService.createObject('object/tangible/wearables/vest/shared_vest_s09.iff', object.getPlanet())
 	necklace = core.objectService.createObject('object/tangible/wearables/necklace/shared_necklace_primitive_03.iff', object.getPlanet())
 	pants = core.objectService.createObject('object/tangible/wearables/pants/shared_pants_s13.iff', object.getPlanet())
-	inventory = object.getSlottedObject('inventory')
 	object._add(shirt)
 	object._add(vest)
 	object._add(necklace)
