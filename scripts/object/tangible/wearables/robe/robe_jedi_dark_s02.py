222dde4b18fb1c1947e78000dc3a9defa3e788e7
Levarrishawk <levarrishawk@gmail.com>
2013-12-04 20:05:27 -0500

    Added stats to Dark Jedi Robes

--- scripts/object/tangible/wearables/robe/robe_jedi_dark_s02.py
+++ scripts/object/tangible/wearables/robe/robe_jedi_dark_s02.py
@@ -2,3 +2,17 @@ import sys
 
 def setup(core, object):
 	return
+	
+def equip(core, actor, target):
+	core.skillModService.addSkillMod(actor, 'constitution_modified', 145)
+	core.skillModService.addSkillMod(actor, 'strength_modified', 145)
+
+	
+	return
+	
+def unequip(core, actor, target):
+	core.skillModService.deductSkillMod(actor, 'constitution_modified', 145)
+	core.skillModService.deductSkillMod(actor, 'strength_modified', 145)
+
+	return
+	
