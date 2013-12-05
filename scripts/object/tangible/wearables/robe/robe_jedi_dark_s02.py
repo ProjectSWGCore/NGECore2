b0a1d2f3d82c507bd1dcda9add4d4b50201d5096
Levarrishawk <levarrishawk@gmail.com>
2013-12-04 20:27:29 -0500

    Added Stats to Light Jedi Robes

--- scripts/object/tangible/wearables/robe/robe_jedi_dark_s02.py
+++ scripts/object/tangible/wearables/robe/robe_jedi_dark_s02.py
@@ -5,14 +5,14 @@ def setup(core, object):
 	
 def equip(core, actor, target):
 	core.skillModService.addSkillMod(actor, 'constitution_modified', 145)
-	core.skillModService.addSkillMod(actor, 'strength_modified', 145)
+	core.skillModService.addSkillMod(actor, 'precision_modified', 145)
 
 	
 	return
 	
 def unequip(core, actor, target):
 	core.skillModService.deductSkillMod(actor, 'constitution_modified', 145)
-	core.skillModService.deductSkillMod(actor, 'strength_modified', 145)
+	core.skillModService.deductSkillMod(actor, 'precision_modified', 145)
 
 	return
 	
--- scripts/object/tangible/wearables/robe/robe_jedi_light_s03.py
+++ scripts/object/tangible/wearables/robe/robe_jedi_light_s03.py
@@ -2,3 +2,18 @@ import sys
 
 def setup(core, object):
 	return
+	
+def equip(core, actor, target):
+	core.skillModService.addSkillMod(actor, 'agility_modified', 185)
+	core.skillModService.addSkillMod(actor, 'constitution_modified', 185)
+	core.skillModService.addSkillMod(actor, 'strength_modified', 185)
+	
+	return
+	
+def unequip(core, actor, target):
+	core.skillModService.deductSkillMod(actor, 'agility_modified', 185)
+	core.skillModService.deductSkillMod(actor, 'constitution_modified', 185)
+	core.skillModService.deductSkillMod(actor, 'strength_modified', 185)
+
+	return
+	
