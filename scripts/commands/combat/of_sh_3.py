bba4153f475f93cddc1dc5373972c3f838d42d63
Ziggeh <ziggy@projectswg.com>
2013-12-08 05:51:03 +0100

    Heal CD reduction is now done through scripts

---
+++ scripts/commands/combat/of_sh_3.py
@@ -0,0 +1,11 @@
+import sys
+import java.util.Random;
+
+def setup(core, actor, target, command):
+	if R
+	else:
+		if actor.getSkillMod('expertise_cooldown_line_of_heal'):
+			command.setCooldown(command.getCooldown() - actor.getSkillMod('expertise_cooldown_line_of_heal').getBase())
+		return
+def run(core, actor, target, commandString):
+	return
