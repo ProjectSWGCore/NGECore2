8b21e32f6afb2caadd2bcbd6859eed7b6a10b889
Ziggeh <ziggy@projectswg.com>
2013-12-08 12:16:26 +0100

    Added First Aid Training (commando)

---
+++ scripts/commands/co_first_aid_training.py
@@ -0,0 +1,9 @@
+import sys
+
+def setup():
+	return
+	
+def run(core, actor, target, commandString):
+
+	core.buffService.addBuffToCreature(actor, 'co_first_aid_training')
+	return
--- scripts/commands/combat/of_sh_3.py
+++ scripts/commands/combat/of_sh_3.py
@@ -1,11 +1,10 @@
 import sys
-import java.util.Random;
 
 def setup(core, actor, target, command):
-	if R
-	else:
-		if actor.getSkillMod('expertise_cooldown_line_of_heal'):
-			command.setCooldown(command.getCooldown() - actor.getSkillMod('expertise_cooldown_line_of_heal').getBase())
-		return
+
+	if actor.getSkillMod('expertise_cooldown_line_of_heal'):
+		command.setCooldown(command.getCooldown() - actor.getSkillMod('expertise_cooldown_line_of_heal').getBase())
+	return
+	
 def run(core, actor, target, commandString):
 	return
