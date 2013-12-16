cd6b2c2678e2b73ed63b8da155152947f34245b4
Treeku <treeku@gmx.com>
2013-12-16 10:04:54 +0000

    Added swoop bike
    - The deed adds a pcd of the swoop to datapad and self-destructs.
    - The pcd stores the objectId of the new swoop.
    - When used, the pcd spawns or destroys the swoop depending on whether
    it exists or not.
    - When the swoop is used or radialed, you can enter/exit it.

--- scripts/object/mobile/vehicle/speederbike_swoop.py
+++ scripts/object/mobile/vehicle/speederbike_swoop.py
@@ -1,4 +1,16 @@
 import sys
 
 def setup(core, object):
+	object.setAttachment('radial_filename', 'vehicle')
+	return
+
+def use(core, actor, object):
+	vehicle = actor.getContainer()
+	
+	if vehicle:
+		if vehicle.getObjectID() == object.getObjectID():
+			vehicle.remove(actor)
+		else:
+			vehicle.add(actor)
+	
 	return
--- scripts/object/tangible/deed/vehicle_deed/speederbike_swoop_deed.py
+++ scripts/object/tangible/deed/vehicle_deed/speederbike_swoop_deed.py
@@ -1,4 +1,19 @@
 import sys
 
 def setup(core, object):
+	object.setAttachment('radial_filename', 'item')
+	return
+
+def use(core, actor, object):
+	pcd = actor.getSlottedObject('datapad')
+	
+	pcd = createObject('object/intangible/vehicle/shared_speederbike_swoop_pcd.iff', actor.getPlanet(), actor.getPosition)
+	
+	if datapad and pcd:
+		datapad.add(pcb)
+		core.objectService.destroyObject(object)
+		vehicle = createObject('object/mobile/vehicle/shared_speederbike_swoop.iff', actor.getPlanet(), actor.getPosition())
+		vehicle.setOwnerId(actor.getObjectID())
+		core.simulationService.add(vehicle, vehicle.getPosition().x, vehicle.getPosition().z, True)
+	
 	return
