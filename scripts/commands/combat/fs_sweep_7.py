af18a56e058e4b44a4eeb8fc961382a74b3c5548
Ziggeh <ziggy@projectswg.com>
2013-12-20 22:49:43 +0100

    Jedi - Action Cost Reduction for Sweep

--- scripts/commands/combat/fs_sweep_7.py
+++ scripts/commands/combat/fs_sweep_7.py
@@ -4,6 +4,11 @@ def setup(core, actor, target, command):
 	if actor.getSkillMod('expertise_damage_line_fs_sweep'):
 		command.setAddedDamage(command.getAddedDamage() + ((command.getAddedDamage() * actor.getSkillMod('expertise_damage_line_fs_sweep').getBase())/100))
 
+	if actor.getSkillMod('expertise_action_line_fs_sweep'):
+		command.setActionCost(command.getActionCost() - ((command.getActionCost() * actor.getSkillMod('expertise_action_line_fs_sweep').getBase())/100))		
+	
 	return
+	
 def run(core, actor, target, commandString):
 	return
+	
