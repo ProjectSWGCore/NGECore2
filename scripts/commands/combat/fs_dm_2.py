38263b72894c966295ab756a954b04f7a5e39e03
Ziggeh <ziggy@projectswg.com>
2013-12-20 20:41:51 +0100

    Jedi - Added Strike Critical Chance

--- scripts/commands/combat/fs_dm_2.py
+++ scripts/commands/combat/fs_dm_2.py
@@ -4,6 +4,9 @@ def setup(core, actor, target, command):
 	if actor.getSkillMod('expertise_damage_line_fs_dm'):
 		command.setAddedDamage(command.getAddedDamage() + ((command.getAddedDamage() * actor.getSkillMod('expertise_damage_line_fs_dm').getBase())/100))
 
+	if actor.getSkillMod('expertise_critical_line_fs_dm'):
+		command.setCriticalChance(command.getCriticalChance() + actor.getSkillMod('expertise_critical_line_fs_dm').getBase())
+
 	return
 def run(core, actor, target, commandString):
 	return
