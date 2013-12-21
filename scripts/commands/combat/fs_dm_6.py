0775adb2d8fe765ed12b4957e49da180292222f6
Ziggeh <ziggy@projectswg.com>
2013-12-21 01:31:00 +0100

    Jedi - Added Strike cooldown reduction modifier

--- scripts/commands/combat/fs_dm_6.py
+++ scripts/commands/combat/fs_dm_6.py
@@ -1,6 +1,10 @@
 import sys
 
 def setup(core, actor, target, command):
+
+	if actor.getSkillMod('expertise_cooldown_line_fs_dm'):
+		command.setCooldown(command.getCooldown() + (actor.getSkillMod('expertise_cooldown_line_fs_dm').getBase()/10))
+
 	if actor.getSkillMod('expertise_damage_line_fs_dm'):
 		command.setAddedDamage(command.getAddedDamage() + ((command.getAddedDamage() * actor.getSkillMod('expertise_damage_line_fs_dm').getBase())/100))
 
