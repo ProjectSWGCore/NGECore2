08d1e68df84676bc163432dd8e091525b19d2b03
Ziggeh <ziggy@projectswg.com>
2013-12-24 18:02:50 +0100

    Revert "Jedi - Soothing Aura now handled in scripts"
    This reverts commit 7a108a9c022bc9dc14fddd8fbe1ae371f010af43.

--- scripts/commands/combat/fs_sh_1.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def setup(core, actor, target, command):
-	if actor.hasBuff('fs_buff_def_1_1') and actor.getSkillMod('expertise_stance_healing_line_fs_heal'):
-		command.setAddedDamage(command.getAddedDamage() + ((command.getAddedDamage() * actor.getSkillMod('expertise_stance_healing_line_fs_heal').getBase())/100))
-
-	return
-def run(core, actor, target, commandString):
-	return
