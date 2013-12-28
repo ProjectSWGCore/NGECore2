4ff7a7cf10dac051d08fd8c687cdf46e7983cbd4
Levarrishawk <levarrishawk@gmail.com>
2013-12-27 23:25:35 -0500

    Preliminary spawning of Mos Eisley Background NPCs
    INCOMPLETE: uninteractible NPCs within Mos Eisley cantina.

--- scripts/static_spawns/tatooine.py
+++ scripts/static_spawns/tatooine.py
@@ -3,6 +3,7 @@ import sys
 def addPlanetSpawns(core, planet):
 
 	stcSvc = core.staticService
+# Wayfar Life Day, by Wefi
 		
 	stcSvc.spawnObject('object/mobile/shared_lifeday_saun_dann.iff', 'tatooine', long(0), float(-5037.00), float(75), float(-6561), float(-0.75), float(0)) # Life Day
 	stcSvc.spawnObject('object/tangible/holiday/life_day/shared_main_lifeday_tree.iff', 'tatooine', long(0), float(-5043.00), float(75), float(-6541.00), float(0.999132931232), float(-0.0416347384453)) # Lifeday Tree
@@ -17,6 +18,109 @@ def addPlanetSpawns(core, planet):
 	bandtat4.setCustomName2('Sunil Eide')
 	stcSvc.spawnObject('object/tangible/instrument/shared_nalargon.iff', 'tatooine', long(0), float(-5045.00), float(75), float(-6555), float(0.999132931232), float(-0.0416347384453)) # Drums
 	stcSvc.spawnObject('object/tangible/instrument/shared_ommni_box.iff', 'tatooine', long(0), float(-5043.00), float(75), float(-6559), float(0.999132931232), float(-0.0416347384453)) # Box
+	
+# Mos Eisley Spawns:  WORK IN PROGRESS by Levarris
+	
+	#Cantina Interior
+	wuher = stcSvc.spawnObject('object/mobile/shared_wuher.iff', 'tatooine', long(1082877), float(8.6), float(-0.9), float(0.4), float(0.71), float(0.71))
+	wuher.setCustomName2('Wuher')
+	
+	chadraFanFemale = stcSvc.spawnObject('object/mobile/shared_chadra_fan_female.iff', 'tatooine', long(1082877), float(10.6), float(-0.9), float(-1.5), float(0.42), float(0.90))
+	chadraFanFemale.setCustomName2('a Chadra Fan Female')
+	
+	chadraFanMale = stcSvc.spawnObject('object/mobile/shared_chadra_fan_male.iff', 'tatooine', long(1082877), float(10.7), float(-0.9), float(-0.3), float(0.64), float(0.77))
+	chadraFanMale.setCustomName2('a Chadra Fan Male')
+	
+	muftak = stcSvc.spawnObject('object/mobile/shared_muftak.iff', 'tatooine', long(1082877), float(20.3), float(-0.9), float(4.9), float(0.82), float(0.57)) 
+	muftak.setCustomName2('Muftak')
+	
+	chissMale1 = stcSvc.spawnObject('object/mobile/ep3/shared_ep3_etyyy_chiss_poacher_01.iff', 'tatooine', long(1082877), float(1.7), float(-0.9), float(-5.0), float(0.71), float(0.71)) 
+	chissMale1.setCustomName2('a Chiss Male')
+	
+	chissMale2 = stcSvc.spawnObject('object/mobile/ep3/shared_ep3_etyyy_chiss_poacher_02.iff', 'tatooine', long(1082877), float(3.4), float(-0.9), float(-4.8), float(0), float(0)) 
+	chissMale2.setCustomName2('a Chiss Male')
+	
+	cantinaStorm = stcSvc.spawnObject('object/mobile/shared_stormtrooper.iff', 'tatooine', long(1082877), float(2.9), float(-0.9), float(-6.5), float(0.71), float(0.71)) 
+	cantinaStorm.setCustomName2('a Stormtrooper')
+	
+	cantinaStormL = stcSvc.spawnObject('object/mobile/shared_stormtrooper_groupleader.iff', 'tatooine', long(1082877), float(3.6), float(-0.9), float(-7.9), float(0.71), float(0.71)) 
+	cantinaStormL.setCustomName2('a Stormtrooper Squad Leader')
+	
+	figrinDan = stcSvc.spawnObject('object/mobile/tatooine_npc/shared_figrin_dan.iff', 'tatooine', long(1082880), float(3.7), float(-0.9), float(-14.4), float(0.42), float(0.91)) 
+	figrinDan.setCustomName2('Figrin Dan')
+	
+	techMor = stcSvc.spawnObject('object/mobile/tatooine_npc/shared_figrin_dan.iff', 'tatooine', long(1082880), float(4.0), float(-0.9), float(-17.0), float(0.42), float(0.91)) 
+	techMor.setCustomName2('Tech M\'or')
+	
+	doikkNats = stcSvc.spawnObject('object/mobile/tatooine_npc/shared_figrin_dan.iff', 'tatooine', long(1082880), float(2.2), float(-0.9), float(-16.4), float(0.42), float(0.91)) 
+	doikkNats.setCustomName2('Doikk Na\'ts')
+	
+	tednDahai = stcSvc.spawnObject('object/mobile/tatooine_npc/shared_figrin_dan.iff', 'tatooine', long(1082880), float(1.3), float(-0.9), float(-15.2), float(0.42), float(0.91)) 
+	tednDahai.setCustomName2('Tedn Dahai')
+	
+	nalanCheel = stcSvc.spawnObject('object/mobile/tatooine_npc/shared_figrin_dan.iff', 'tatooine', long(1082880), float(0.5), float(-0.9), float(-17.1), float(0.42), float(0.91)) 
+	nalanCheel.setCustomName2('Nalan Cheel')
+	
+	businessman1 = stcSvc.spawnObject('object/mobile/shared_dressed_businessman_human_male_01.iff', 'tatooine', long(1082877), float(10.7), float(-0.9), float(1.9), float(0.42), float(0.91)) 
+	businessman1.setCustomName2('a Businessman')
+	
+	commoner1 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_tatooine_aqualish_male_01.iff', 'tatooine', long(1082877), float(10.3), float(-0.9), float(2.7), float(0.42), float(0.91)) 
+	commoner1.setCustomName2('a Commoner')
+	
+	entertainer1 = stcSvc.spawnObject('object/mobile/shared_dressed_entertainer_trainer_twk_female_01.iff', 'tatooine', long(1082877), float(9.4), float(-0.9), float(3.9), float(0.42), float(0.91)) 
+	entertainer1.setCustomName2('an Entertainer')
+	
+	noble1 = stcSvc.spawnObject('object/mobile/shared_dressed_noble_trandoshan_male_01.iff', 'tatooine', long(1082877), float(8.6), float(-0.9), float(4.8), float(0.42), float(0.91)) 
+	noble1.setCustomName2('a Noble')
+	
+	commoner2 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_tatooine_trandoshan_female_01.iff', 'tatooine', long(1082877), float(4.1), float(-0.9), float(5.7), float(0.42), float(0.91)) 
+	commoner2.setCustomName2('a Commoner')
+	
+	commoner3 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_bothan_female_01.iff', 'tatooine', long(1082877), float(3.1), float(-0.9), float(5.9), float(0.42), float(0.91)) 
+	commoner3.setCustomName2('a Commoner')
+	
+	commoner4 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_fat_twilek_male_01.iff', 'tatooine', long(1082877), float(1.7), float(-0.9), float(6.0), float(0.42), float(0.91)) 
+	commoner4.setCustomName2('a Commoner')
+	
+	commoner5 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_bothan_male_01.iff', 'tatooine', long(1082877), float(-0.4), float(-0.9), float(5.9), float(0.42), float(0.91)) 
+	commoner5.setCustomName2('a Commoner')
+	
+	commoner6 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_human_male_05.iff', 'tatooine', long(1082877), float(16.0), float(-0.9), float(4.1), float(0.42), float(0.91)) 
+	commoner6.setCustomName2('a Commoner')
+	
+	commoner7 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_fat_zabrak_male_01.iff', 'tatooine', long(1082877), float(8.8), float(-0.9), float(-6.0), float(0.42), float(0.91)) 
+	commoner7.setCustomName2('a Patron')
+	
+	commoner2 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_human_male_08.iff', 'tatooine', long(1082877), float(6.8), float(-0.9), float(-6.5), float(0.42), float(0.91)) 
+	commoner2.setCustomName2('a Commoner')
+	
+	#Starport Interior
+	
+	#City Hall Interior
+	
+	#Lucky Despot Interior
+	
+	#Medical Center Interior
+	
+	#Theater Interior
+	
+	#Miscellaneous Building Interiors
+	
+	#Outside
+	
+# Mos Espa NPC Spawns
 
+# Mos Entha NPC Spawns
+
+# Bestine NPC Spawns
+
+# Anchorhead NPC Spawns
+
+# Mos Taike NPC Spawns
+
+# Wayfar NPC Spawns
+
+# Jabba's Palace Theme Park Spawns	
+	
 	return
 	
