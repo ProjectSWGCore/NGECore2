5a9f8f88064e9f9aada71e58cbb8c4293d65a2b0
Fingies <impcreationswg@gmail.com>
2013-12-28 16:18:08 -0800

    modified life day spawns to include vendor

--- scripts/static_spawns/tatooine.py
+++ scripts/static_spawns/tatooine.py
@@ -3,7 +3,7 @@ import sys
 def addPlanetSpawns(core, planet):
 
 	stcSvc = core.staticService
-# Wayfar Life Day, by Wefi
+# Wayfar Life Day, by Wefi, modified by Fingies
 		
 	stcSvc.spawnObject('object/mobile/shared_lifeday_saun_dann.iff', 'tatooine', long(0), float(-5037.00), float(75), float(-6561), float(-0.75), float(0)) # Life Day
 	stcSvc.spawnObject('object/tangible/holiday/life_day/shared_main_lifeday_tree.iff', 'tatooine', long(0), float(-5043.00), float(75), float(-6541.00), float(0.999132931232), float(-0.0416347384453)) # Lifeday Tree
