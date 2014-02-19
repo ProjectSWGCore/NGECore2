11555b99cf7328c7808ba4d55df350cb71609bc4
Waverunner <waverun@live.com>
2014-02-19 18:31:03 -0500

    Guild names now show when examining another player

---
+++ src/protocol/swg/GuildRequestMessage.java
@@ -0,0 +1,34 @@
+package protocol.swg;
+
+import org.apache.mina.core.buffer.IoBuffer;
+
+import resources.common.Console;
+import resources.common.StringUtilities;
+
+public class GuildRequestMessage extends SWGMessage {
+
+	private long characterId;
+	
+	public GuildRequestMessage() {
+	}
+	
+	@Override
+	public void deserialize(IoBuffer data) {
+		//data.getShort();
+		setCharacterId(data.getLong());
+	}
+
+	@Override
+	public IoBuffer serialize() {
+		return null;
+	}
+
+	public long getCharacterId() {
+		return characterId;
+	}
+
+	public void setCharacterId(long characterId) {
+		this.characterId = characterId;
+	}
+
+}
--- src/resources/common/Opcodes.java
+++ src/resources/common/Opcodes.java
@@ -59,5 +59,6 @@ public class Opcodes {
 	public static int FactionRequestMessage = CRC.StringtoCRC("FactionRequestMessage");
 	public static int FactionResponseMessage = CRC.StringtoCRC("FactionResponseMessage");
 	public static int SetWaypointColor = CRC.StringtoCRC("SetWaypointColor");
+	public static int GuildRequestMessage = CRC.StringtoCRC("GuildRequestMessage");
 	
 }
