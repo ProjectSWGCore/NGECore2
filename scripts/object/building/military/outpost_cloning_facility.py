02ef99900a1a7cb37b0b1df24739211ac0acb3b4
Levarrishawk <levarrishawk@gmail.com>
2014-03-15 21:45:09 -0400

    Fixed recursion error: AGAIN!
    Same error as last time.

--- scripts/object/building/military/outpost_cloning_facility.py
+++ scripts/object/building/military/outpost_cloning_facility.py
@@ -7,7 +7,7 @@ def setup(core, object):
 	objSvc.createChildObject(object, 'object/tangible/terminal/shared_terminal_cloning.iff', float(0.04), float(0.16), float(-6), float(-0.06), float(0.99), 3)
 		
 	spawnPoints = Vector()
-	spawnPoints.add(SpawnPoint(3.2408, 0.125266, -3.59732, 0.707641, -0.706572, 5))
+	spawnPoints.add(SpawnPoint(3.24, 0.16, -3.60, 0.71, -0.71, 5))
 		
 	object.setAttachment('spawnPoints', spawnPoints)
 
