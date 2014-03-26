da33dba33e214ae0283a08c0816d11e6bf447fd8
Bronco69 <petertigges1977@googlemail.com>
2014-03-26 22:34:43 +0100

    Fixed BH + Commando armor upgrades
    - pinpoint shielding for commando expertise fixed
    - energy + kinetic for BH expertise fixed

--- scripts/skillMods/expertise_innate_protection_energy.py
+++ scripts/skillMods/expertise_innate_protection_energy.py
@@ -1,10 +1,12 @@
 import sys
 
 def add(core, actor, name, base):
+	actor.addSkillMod('energy', base)
 	actor.addSkillMod(name, base)
 	return
 	
 def deduct(core, actor, name, base):
+	actor.deductSkillMod('energy', base)
 	actor.deductSkillMod(name, base)
 	return
 	
