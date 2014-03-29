7376be7b954ba702e9904ff4e1df319cfadd0ec1
Light2 <light_pswg@yahoo.com>
2014-03-29 18:42:24 +0100

    fixed bug where character wouldnt gain any abilities on using the frog to get lvl 90

--- src/services/PlayerService.java
+++ src/services/PlayerService.java
@@ -598,7 +598,7 @@ public class PlayerService implements INetworkDispatch {
 		
 		core.buffService.clearBuffs(creature);
 		
-		for (String skill : new ArrayList<String>(creature.getSkills())) {
+		for (String skill : new ArrayList<String>(creature.getSkills().get())) {
 			core.skillService.removeSkill(creature, skill);
 		}
 		
