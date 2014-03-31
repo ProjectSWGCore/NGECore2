2ac9872498fdb723fa580d3a70e3c399672b9317
Treeku <treeku@gmx.com>
2014-03-31 17:29:10 +0100

    Speedbuff fix moved from java to skillMod script

--- scripts/skillMods/movement.py
+++ scripts/skillMods/movement.py
@@ -2,10 +2,12 @@ import sys
 
 def add(core, actor, name, base):
 	actor.addSkillMod(name, base)
+	actor.setSpeedMultiplierMod(actor.getSpeedMultiplierMod() - (float(base) / float(10)))
 	actor.setSpeedMultiplierBase(actor.getSpeedMultiplierBase() + base / 100)
 	return
 
 def deduct(core, actor, name, base):
 	actor.setSpeedMultiplierBase(actor.getSpeedMultiplierBase() - base / 100)
+	actor.setSpeedMultiplierMod(actor.getSpeedMultiplierMod() - (float(base) / float(10)))
 	actor.deductSkillMod(name, base)
 	return
