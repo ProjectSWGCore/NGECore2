9341998385f9ddd85a80e6098dc3011ba3f93c8f
Bronco69 <petertigges1977@googlemail.com>
2014-04-05 03:59:20 +0200

    Bounty Hunter - expertise scripts reworked
    All commands and skills for expertise changed and checked

--- scripts/expertise/expertise_bh_agility_2.py
+++ scripts/expertise/expertise_bh_agility_2.py
@@ -1,48 +1,9 @@
 import sys
 
-def addExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'bounty_hunter_1a':
-		return
-
-	actor.addSkill('expertise_bh_agility_2')
-
-	actor.addSkillMod('agility_modified', 25)
-
-	addAbilities(core, actor, player)
-
-	return
-
-def removeExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'bounty_hunter_1a':
-		return
-
-	actor.removeSkill('expertise_bh_agility_2')
-
-	actor.removeSkillMod('agility_modified', 25)
-
-	removeAbilities(core, actor, player)
-
-	return
-
-# this checks what abilities the player gets by level, need to also call this on level-up
 def addAbilities(core, actor, player):
-
-
+	actor.addAbility("expertise_bh_agility_1")
 	return
 
 def removeAbilities(core, actor, player):
-
-
+	actor.removeAbility("expertise_bh_agility_1")
 	return
--- scripts/expertise/expertise_bh_man_crit_1.py
+++ scripts/expertise/expertise_bh_man_crit_1.py
@@ -1,50 +1,9 @@
 import sys
 
-def addExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'bounty_hunter_1a':
-		return
-
-	actor.addSkill('expertise_bh_man_crit_1')
-
-	actor.addSkillMod('expertise_strikethrough_chance', 2)
-	actor.addSkillMod('expertise_bh_detect_camo_chance', 10)
-
-	addAbilities(core, actor, player)
-
-	return
-
-def removeExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'bounty_hunter_1a':
-		return
-
-	actor.removeSkill('expertise_bh_man_crit_1')
-
-	actor.removeSkillMod('expertise_strikethrough_chance', 2)
-	actor.removeSkillMod('expertise_bh_detect_camo_chance', 10)
-
-	removeAbilities(core, actor, player)
-
-	return
-
-# this checks what abilities the player gets by level, need to also call this on level-up
 def addAbilities(core, actor, player):
-
-
+	actor.addAbility("expertise_bh_man_crit_1")
 	return
 
 def removeAbilities(core, actor, player):
-
-
+	actor.removeAbility("expertise_bh_man_crit_1")
 	return
