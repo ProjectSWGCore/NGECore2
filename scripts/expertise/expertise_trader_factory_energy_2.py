acd709f9ce486e7e8766208c34d4d0ffc1fd59c9
Bronco69 <petertigges1977@googlemail.com>
2014-04-05 06:42:28 +0200

    Trader - expertise scripts reworked
    All commands and skills for expertise changed and checked

--- scripts/expertise/expertise_trader_factory_energy_2.py
+++ scripts/expertise/expertise_trader_factory_energy_2.py
@@ -1,48 +1,9 @@
 import sys
 
-def addExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'trader_1a':
-		return
-
-	actor.addSkill('expertise_trader_factory_energy_2')
-
-	actor.addSkillMod('expertise_factory_energy_decrease', 5)
-
-	addAbilities(core, actor, player)
-
-	return
-
-def removeExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'trader_1a':
-		return
-
-	actor.removeSkill('expertise_trader_factory_energy_2')
-
-	actor.removeSkillMod('expertise_factory_energy_decrease', 5)
-
-	removeAbilities(core, actor, player)
-
-	return
-
-# this checks what abilities the player gets by level, need to also call this on level-up
 def addAbilities(core, actor, player):
-
-
+	actor.addAbility("expertise_trader_factory_energy_2")
 	return
 
 def removeAbilities(core, actor, player):
-
-
+	actor.removeAbility("expertise_trader_factory_energy_2")
 	return
