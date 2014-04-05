1da76d500b81a0362a046e0f50e07d65178cadeb
Bronco69 <petertigges1977@googlemail.com>
2014-04-05 05:28:59 +0200

    Munition - expertise scripts reworked
    All commands and skills for expertise changed and checked

--- scripts/expertise/expertise_munition_weaponsmith_dexterity_2.py
+++ scripts/expertise/expertise_munition_weaponsmith_dexterity_2.py
@@ -1,48 +1,9 @@
 import sys
 
-def addExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'trader_mun_1a':
-		return
-
-	actor.addSkill('expertise_munition_weaponsmith_dexterity_2')
-
-	actor.addSkillMod('weapon_assembly', 5)
-
-	addAbilities(core, actor, player)
-
-	return
-
-def removeExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'trader_mun_1a':
-		return
-
-	actor.removeSkill('expertise_munition_weaponsmith_dexterity_2')
-
-	actor.removeSkillMod('weapon_assembly', 5)
-
-	removeAbilities(core, actor, player)
-
-	return
-
-# this checks what abilities the player gets by level, need to also call this on level-up
 def addAbilities(core, actor, player):
-
-
+	actor.addAbility("expertise_munition_weaponsmith_dexterity_2")
 	return
 
 def removeAbilities(core, actor, player):
-
-
+	actor.removeAbility("expertise_munition_weaponsmith_dexterity_2")
 	return
