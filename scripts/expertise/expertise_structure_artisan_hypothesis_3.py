10896fe4e1980b4bdd93a4db1928091a77cfde77
Bronco69 <petertigges1977@googlemail.com>
2014-04-05 05:30:43 +0200

    Structure - expertise scripts reworked
    All commands and skills for expertise changed and checked

--- scripts/expertise/expertise_structure_artisan_hypothesis_3.py
+++ scripts/expertise/expertise_structure_artisan_hypothesis_3.py
@@ -1,48 +1,9 @@
 import sys
 
-def addExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'trader_struct_1a':
-		return
-
-	actor.addSkill('expertise_structure_artisan_hypothesis_3')
-
-	actor.addSkillMod('expertise_experimentation_increase_artisan', 3)
-
-	addAbilities(core, actor, player)
-
-	return
-
-def removeExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'trader_struct_1a':
-		return
-
-	actor.removeSkill('expertise_structure_artisan_hypothesis_3')
-
-	actor.removeSkillMod('expertise_experimentation_increase_artisan', 3)
-
-	removeAbilities(core, actor, player)
-
-	return
-
-# this checks what abilities the player gets by level, need to also call this on level-up
 def addAbilities(core, actor, player):
-
-
+	actor.addAbility("expertise_structure_artisan_hypothesis_3")
 	return
 
 def removeAbilities(core, actor, player):
-
-
+	actor.removeAbility("expertise_structure_artisan_hypothesis_3")
 	return
