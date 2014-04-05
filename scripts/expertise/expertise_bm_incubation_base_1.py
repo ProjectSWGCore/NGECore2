d7756e185b72f343734d0bea7097e02305735fb6
Bronco69 <petertigges1977@googlemail.com>
2014-04-05 04:12:22 +0200

    Beast Master - expertise scripts reworked
    All commands and skills for expertise changed and checked

--- scripts/expertise/expertise_bm_incubation_base_1.py
+++ scripts/expertise/expertise_bm_incubation_base_1.py
@@ -1,50 +1,9 @@
 import sys
 
-def addExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'all_1a':
-		return
-
-	actor.addSkill('expertise_bm_incubation_base_1')
-
-	actor.addSkillMod('expertise_bm_base_mod', 100)
-
-	addAbilities(core, actor, player)
-
-	return
-
-def removeExpertisePoint(core, actor):
-
-	player = actor.getSlottedObject('ghost')
-
-	if not player:
-		return
-
-	if not player.getProfession() == 'all_1a':
-		return
-
-	actor.removeSkill('expertise_bm_incubation_base_1')
-
-	actor.removeSkillMod('expertise_bm_base_mod', 100)
-
-	removeAbilities(core, actor, player)
-
-	return
-
-# this checks what abilities the player gets by level, need to also call this on level-up
 def addAbilities(core, actor, player):
-
-	actor.addAbility('bm_collect_dna')
-
+	actor.addAbility("bm_collect_dna")
 	return
 
 def removeAbilities(core, actor, player):
-
-	actor.removeAbility('bm_collect_dna')
-
+	actor.removeAbility("bm_collect_dna")
 	return
