348b6b7f553b1118daec06da6ce9a0ff7c65e5a4
CharonInferar <CharonInferar@aol.de>
2014-04-06 04:44:40 +0200

    Cleanup of options.cfg
    Used kneel py for spawning tools, replaced it with original

--- scripts/commands/kneel.py
+++ scripts/commands/kneel.py
@@ -4,25 +4,7 @@ def setup():
 	return
 	
 def run(core, actor, target, commandString):
-	
-	#commandString='object/tangible/survey_tool/shared_survey_tool_mineral.iff';
-	commandString='object/tangible/survey_tool/shared_survey_tool_lumber.iff';
-	#commandString='object/tangible/survey_tool/shared_survey_tool_inorganic.iff';
-	#commandString='object/tangible/survey_tool/shared_survey_tool_gas.iff';
-	#commandString='object/tangible/survey_tool/shared_survey_tool_solar.iff';
-	#commandString='object/tangible/crafting/station/shared_generic_tool.iff';
-	object = core.objectService.createObject(commandString, actor.getPlanet())
-	if not object:
-		return
-		
-	inventory = actor.getSlottedObject('inventory')
-	
-	if inventory:
-		inventory.add(object)
-	
-	#core.objectService.loadResources()
-	#core.objectService.loadResources2()
-	
+
 	if actor.getPosture() == 13 or actor.getPosture() == 14:
 		return
 
