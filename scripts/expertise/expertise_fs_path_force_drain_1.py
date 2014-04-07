01fedd5cdbaa4567b25b2e95d41848af1c0bfff4
Bronco69 <petertigges1977@googlemail.com>
2014-04-07 03:18:46 +0200

    fixes in Jedi expertise
    multi expertise commands fixed

--- scripts/expertise/expertise_fs_path_force_drain_1.py
+++ scripts/expertise/expertise_fs_path_force_drain_1.py
@@ -1,9 +1,22 @@
 import sys
 
 def addAbilities(core, actor, player):
-	actor.addAbility("fs_drain_1")
+	if actor.getLevel() >= 26:
+		actor.addAbility("fs_drain_1")
+	if actor.getLevel() >= 34:
+		actor.addAbility("fs_drain_2")
+	if actor.getLevel() >= 48:
+		actor.addAbility("fs_drain_3")
+	if actor.getLevel() >= 62:
+		actor.addAbility("fs_drain_4")
+	if actor.getLevel() >= 76:
+		actor.addAbility("fs_drain_5")
 	return
 
 def removeAbilities(core, actor, player):
 	actor.removeAbility("fs_drain_1")
+	actor.removeAbility("fs_drain_2")
+	actor.removeAbility("fs_drain_3")
+	actor.removeAbility("fs_drain_4")
+	actor.removeAbility("fs_drain_5")
 	return
--- scripts/expertise/expertise_fs_path_saber_reflect_1.py
+++ scripts/expertise/expertise_fs_path_saber_reflect_1.py
@@ -1,9 +1,9 @@
 import sys
 
 def addAbilities(core, actor, player):
-	actor.addAbility("fs_saber_reflect_buff")
+	actor.addAbility("fs_saber_reflect")
 	return
 
 def removeAbilities(core, actor, player):
-	actor.removeAbility("fs_saber_reflect_buff")
+	actor.removeAbility("fs_saber_reflect")
 	return
