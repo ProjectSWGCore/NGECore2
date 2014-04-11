4df7274ac9bcb813a7cfc6af4680ca3db80a33f8
CharonInferar <CharonInferar@aol.de>
2014-04-11 04:14:29 +0200

    Structure terminal work
    Creation of player_house_deed class
    terminal menus made
    py scripts arranged

--- scripts/object/tangible/deed/player_house_deed/generic_house_small_deed.py
+++ scripts/object/tangible/deed/player_house_deed/generic_house_small_deed.py
@@ -1,7 +1,11 @@
 import sys
 
 def setup(core, object):
-	object.setAttachment('radial_filename', 'structureDeed');
+	object.setAttachment('radial_filename', 'structureDeed')
+	object.setConstructorTemplate('object/building/player/construction/shared_construction_player_house_corellia_small_style_01.iff')
+	object.setStructureTemplate('object/tangible/deed/player_house_deed/shared_generic_house_small_deed.iff')
+	object.setLotRequirement(1)
+	object.setBMR(15)
 	return
 
 def use(core, actor, object):
