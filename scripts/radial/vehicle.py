13495e3f3501150363be7dbdec3d0f54c74c8b48
CharonInferar <CharonInferar@aol.de>
2014-04-13 02:22:45 +0200

    first work on swoop deed

--- scripts/radial/vehicle.py
+++ scripts/radial/vehicle.py
@@ -1,4 +1,6 @@
 from resources.common import RadialOptions
+from resources.common import RadialOptions
+from services.ai import Vehicles
 import sys
 
 def createRadial(core, owner, target, radials):
@@ -11,7 +13,16 @@ def createRadial(core, owner, target, radials):
 def handleSelection(core, owner, target, option):
 	if target:
 		if option == 21 or option == 40:
-			core.objectService.useObject(owner, target)
+			#owner.setPosture(16)
+			owner.setMounted(1)
+			#owner.setParent(target)
+			target.initMount(owner)	
+			#target.add(owner)
+			#target.setOwner(owner)
+			#Vehicles.testbike(owner,target)
+			
+			#target.unmount(owner)
+			#core.objectService.useObject(owner, target)
 		if option == 60:
 			core.objectService.destroyObject(target)
 	return
