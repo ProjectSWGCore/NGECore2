1b1294136a92ff147dbe184e10457923938f5a51
Seefo <rezen1911@gmail.com>
2014-04-13 13:37:54 -0400

    Fixed a bunch of vehicle bugs - will probably change the way vehicles are being handled soon to account for other vehicle templates

--- scripts/radial/vehicle.py
+++ scripts/radial/vehicle.py
@@ -4,14 +4,18 @@ import sys
 
 def createRadial(core, owner, target, radials):
 	radials.add(RadialOptions(0, 7, 1, ''))
-	radials.add(RadialOptions(1, 40, 3, '@pet/pet_menu:menu_enter_exit'))
-	radials.add(RadialOptions(0, 21, 1, ''))
-	radials.add(RadialOptions(0, 60, 3, '@pet/pet_menu:menu_store'))
+	
+	if owner.canMount(target):
+		radials.add(RadialOptions(0, 208, 1, '@pet/pet_menu:menu_enter_exit'))
+		
+	if target.getOwnerId() == owner.getObjectID():
+		radials.add(RadialOptions(0, 60, 3, '@pet/pet_menu:menu_store'))
+		
 	return
 	
 def handleSelection(core, owner, target, option):
 	if target:
-		if option == 21 or option == 40:
+		if option == 208:
 			if not owner.isMounted():
 				owner.setMounted(1)
 				target.initMount(owner)	
@@ -19,5 +23,7 @@ def handleSelection(core, owner, target, option):
 				target.unmount(owner)
 			
 		if option == 60:
+			if owner.isMounted(): vehicleObject.unmount(owner)
 			core.objectService.destroyObject(target)
+			owner.setAttachment('activeVehicleID', None)
 	return
