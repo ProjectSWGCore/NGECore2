d3215407f39cd29e4282b2a2543d8803b17c905a
Ziggeh <ziggy@projectswg.com>
2014-04-15 17:38:28 +0200

    Weapons - Created datatable of WeaponTypes

---
+++ src/resources/datatables/WeaponType.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2013 <Project SWG>
+ * 
+ * This File is part of NGECore2.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Using NGEngine to work with NGECore2 is making a combined work based on NGEngine. 
+ * Therefore all terms and conditions of the GNU Lesser General Public License cover the combination.
+ ******************************************************************************/
+package resources.datatables;
+
+public class WeaponType {
+	
+	public static int RIFLE = 0;
+	public static int CARBINE = 1;
+	public static int PISTOL = 2;
+	public static int ONEHANDEDMELEE = 4;
+	public static int TWOHANDEDMELEE = 5;
+	public static int UNARMED = 6;
+	public static int POLEARMMELEE = 7;
+	public static int THROWN = 8;
+	public static int ONEHANDEDSABER = 9;
+	public static int TWOHANDEDSABER = 10;
+	public static int POLEARMSABER = 11;
+	public static int HEAVYWEAPON = 12;
+	public static int FLAMETHROWER = 13;
+	
+}
