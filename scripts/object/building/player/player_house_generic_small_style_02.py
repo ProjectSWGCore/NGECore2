9975656a97367a69ff389584739095fb590952cb
tacef <neuwirth.christoph@gmx.net>
2014-04-18 18:16:36 +0200

    Added Generic House Small Style 02

--- scripts/object/building/player/player_house_generic_small_style_02.py
+++ scripts/object/building/player/player_house_generic_small_style_02.py
@@ -1,4 +1,11 @@
 import sys
 
 def setup(core, object):
+	sign = core.objectService.createChildObject(object, 'object/tangible/sign/player/shared_house_address.iff', -1.9, 2.86, 8.35, -1, 0, -1)
+	print(sign)
+	object.setAttachment("structureSign", sign)
+	
+	structureterminal = core.objectService.createChildObject(object, 'object/tangible/terminal/shared_terminal_player_structure.iff', 6.5, 0.5, -3.8, 0.707108, -0.707108, 3)
+	#structureterminal.setAttachment('radial_filename', 'structure_management_terminal')
+	structureterminal.setAttachment('housing_parentstruct', object)
 	return
