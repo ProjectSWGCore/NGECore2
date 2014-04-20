ee66912e8c023120f0342feba44724c2b7ae5a71
tacef <neuwirth.christoph@gmx.net>
2014-04-20 22:00:25 +0200

    Added more Houses
    Added large, medium and small corellian houses
    Added large and medium generic houses
    Fixed old Houses to be more accurate to how it was
    Fixed DevService and removed unneeded houses(only need 1 for testing)

--- scripts/object/building/player/player_house_corellia_large_style_02.py
+++ scripts/object/building/player/player_house_corellia_large_style_02.py
@@ -1,4 +1,12 @@
 import sys
 
+
 def setup(core, object):
+	sign = core.objectService.createChildObject(object, 'object/tangible/sign/player/shared_house_address.iff', -13.7, 3, 9.1, -1, 0, -1)
+	print(sign)
+	object.setAttachment("structureSign", sign)
+	
+	structureterminal = core.objectService.createChildObject(object, 'object/tangible/terminal/shared_terminal_player_structure.iff', -10.5, 0.7, 4.5, 0.707108, -0.707108, 1)
+	#structureterminal.setAttachment('radial_filename', 'structure_management_terminal')
+	structureterminal.setAttachment('housing_parentstruct', object)
 	return
