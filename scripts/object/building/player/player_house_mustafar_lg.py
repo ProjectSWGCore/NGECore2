061adee910e3ac506c901e64257deb764ffa33c2
tacef <neuwirth.christoph@gmx.net>
2014-04-25 17:42:05 +0200

    Continued work on Playerhouse deeds
    Added some more houses(need more testing when elevators work)
    Also altered objectservice to work with tcg houses

--- scripts/object/building/player/player_house_mustafar_lg.py
+++ scripts/object/building/player/player_house_mustafar_lg.py
@@ -1,4 +1,11 @@
 import sys
 
 def setup(core, object):
+	sign = core.objectService.createChildObject(object, 'object/tangible/sign/player/shared_house_address.iff', 1.5, 2.86, 13.0, -1, 0, -1)
+	print(sign)
+	object.setAttachment("structureSign", sign)
+	
+	structureterminal = core.objectService.createChildObject(object, 'object/tangible/terminal/shared_terminal_player_structure.iff', -4.1, 0.55, 6.6, 0.714 ,0.714, 1)
+	#structureterminal.setAttachment('radial_filename', 'structure_management_terminal')
+	structureterminal.setAttachment('housing_parentstruct', object)
 	return
