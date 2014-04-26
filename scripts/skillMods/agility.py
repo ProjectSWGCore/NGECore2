563f2572bf0903dd8efefbfd0075e8343c33fcc3
zingzing175 <jeremy@zingstecharchive.com>
2014-04-26 16:21:30 -0700

    Revert "Revert "Fixed agility.py issue""
    This reverts commit 3e02e080cf485e254f820f75d7591c32c1b3a145.

--- scripts/skillMods/agility.py
+++ scripts/skillMods/agility.py
@@ -1,14 +1,14 @@
 import sys
 
-def add(core, actor, skillMod, value):
-	core.skillModService.addSkillMod('display_only_dodge', skillMod.getBase())
-	core.skillModService.addSkillMod('display_only_parry', skillMod.getBase() / 2)
-	core.skillModService.addSkillMod('display_only_evasion', skillMod.getBase())
+def add(core, actor, skillMod, divisor):
+	core.skillModService.addSkillMod(actor, 'display_only_dodge', skillMod.getBase())
+	core.skillModService.addSkillMod(actor, 'display_only_parry', skillMod.getBase() / 2)
+	core.skillModService.addSkillMod(actor, 'display_only_evasion', skillMod.getBase())
 	return
 
-def deduct(core, actor, skillMod, value):
-	core.skillModService.deductSkillMod('display_only_dodge', skillMod.getBase())
-	core.skillModService.deductSkillMod('display_only_parry', skillMod.getBase() / 2)
-	core.skillModService.deductSkillMod('display_only_evasion', skillMod.getBase())
+def deduct(core, actor, skillMod, divisor):
+	core.skillModService.deductSkillMod(actor, 'display_only_dodge', skillMod.getBase())
+	core.skillModService.deductSkillMod(actor, 'display_only_parry', skillMod.getBase() / 2)
+	core.skillModService.deductSkillMod(actor, 'display_only_evasion', skillMod.getBase())
 	return
 	
