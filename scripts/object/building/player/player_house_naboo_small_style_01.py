41589dd0cbebc652f41527760c38ddc0fe54b193
tacef <neuwirth.christoph@gmx.net>
2014-04-26 09:27:31 +0200

    Naboo House Deed's added
    Also fixed typos corellia house deed's

--- scripts/object/building/player/player_house_naboo_small_style_01.py
+++ scripts/object/building/player/player_house_naboo_small_style_01.py
@@ -1,4 +1,11 @@
 import sys
 
 def setup(core, object):
+	sign = core.objectService.createChildObject(object, 'object/tangible/sign/player/shared_house_address.iff', -1.7, 3, -8.2, 0, 1, -1)
+	print(sign)
+	object.setAttachment("structureSign", sign)
+	
+	structureterminal = core.objectService.createChildObject(object, 'object/tangible/terminal/shared_terminal_player_structure.iff', 0, 0.7, 6.2, 1, 0, 1)
+	#structureterminal.setAttachment('radial_filename', 'structure_management_terminal')
+	structureterminal.setAttachment('housing_parentstruct', object)
 	return
