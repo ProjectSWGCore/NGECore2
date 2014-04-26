9b3397923afd7272dda6f4f95977b41998df07f2
Treeku <treeku@gmx.com>
2014-04-26 01:07:40 +0100

    Fixed Agility.py

--- scripts/skillMods/agility.py
+++ scripts/skillMods/agility.py
@@ -1,14 +1,14 @@
 import sys
 
 def add(core, actor, skillMod, value):
-	core.skillModService.addSkillMod('display_only_dodge', base)
-	core.skillModService.addSkillMod('display_only_parry', base / 2)
-	core.skillModService.addSkillMod('display_only_evasion', base)
+	core.skillModService.addSkillMod('display_only_dodge', skillMod.getBase())
+	core.skillModService.addSkillMod('display_only_parry', skillMod.getBase() / 2)
+	core.skillModService.addSkillMod('display_only_evasion', skillMod.getBase())
 	return
 
 def deduct(core, actor, skillMod, value):
-	core.skillModService.deductSkillMod('display_only_dodge', base)
-	core.skillModService.deductSkillMod('display_only_parry', base / 2)
-	core.skillModService.deductSkillMod('display_only_evasion', base)
+	core.skillModService.deductSkillMod('display_only_dodge', skillMod.getBase())
+	core.skillModService.deductSkillMod('display_only_parry', skillMod.getBase() / 2)
+	core.skillModService.deductSkillMod('display_only_evasion', skillMod.getBase())
 	return
 	
