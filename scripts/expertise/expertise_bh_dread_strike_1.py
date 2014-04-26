4c29a54689995880e31db2f567e72d10d1cb3517
Ziggeh <ziggy@projectswg.com>
2014-04-26 19:22:50 +0200

    Bounty Hunter - Fixed Dread Strike expertise
    It was granting unused abilities

--- scripts/expertise/expertise_bh_dread_strike_1.py
+++ scripts/expertise/expertise_bh_dread_strike_1.py
@@ -1,22 +1,9 @@
 import sys
 
 def addAbilities(core, actor, player):
-	if actor.getLevel() >= 26:
-		actor.addAbility("bh_dread_strike_1")
-	if actor.getLevel() >= 34:
-		actor.addAbility("bh_dread_strike_2")
-	if actor.getLevel() >= 48:
-		actor.addAbility("bh_dread_strike_3")
-	if actor.getLevel() >= 62:
-		actor.addAbility("bh_dread_strike_4")
-	if actor.getLevel() >= 76:
-		actor.addAbility("bh_dread_strike_5")
+	actor.addAbility("bh_dread_strike_1")
 	return
 
 def removeAbilities(core, actor, player):
 	actor.removeAbility("bh_dread_strike_1")
-	actor.removeAbility("bh_dread_strike_2")
-	actor.removeAbility("bh_dread_strike_3")
-	actor.removeAbility("bh_dread_strike_4")
-	actor.removeAbility("bh_dread_strike_5")
 	return
