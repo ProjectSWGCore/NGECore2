9fc30d10c4b780d0bc5ae70279b85b3ac05c61f5
Treeku <treeku@gmx.com>
2014-04-28 22:07:32 +0100

    Removed DeedObject, made buildings simpler

--- scripts/commands/resourceemptyhopper.py
+++ scripts/commands/resourceemptyhopper.py
@@ -2,9 +2,7 @@ import sys
 
 def setup():
     return
-    
+
 def run(core, actor, target, commandString):
     core.harvesterService.handleHarvesterDeactivateCommand(actor, target, commandString)
     return
-
-	
--- scripts/object/tangible/deed/city_deed/cityhall_tatooine_deed.py
+++ scripts/object/tangible/deed/city_deed/cityhall_tatooine_deed.py
@@ -2,10 +2,6 @@ import sys
 
 def setup(core, object):
 	object.setAttachment('radial_filename', 'deeds/structureDeed')
-	object.setConstructorTemplate('object/building/player/shared_construction_structure.iff')
-	object.setStructureTemplate('object/tangible/deed/city_deed/shared_cityhall_tatooine_deed.iff')
-	#object.setLotRequirement(5)
-	object.setBMR(308)
 	return
 
 def use(core, actor, object):
