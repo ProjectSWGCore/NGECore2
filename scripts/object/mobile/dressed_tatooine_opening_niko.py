e111724115b8f61837cd198575d639c7a0640fb1
tacef <neuwirth.christoph@gmx.net>
2014-05-01 18:25:39 +0200

    Added NPC scripts( No conversations yet)

--- scripts/object/mobile/dressed_tatooine_opening_niko.py
+++ scripts/object/mobile/dressed_tatooine_opening_niko.py
@@ -1,4 +1,10 @@
 import sys
+from resources.datatables import Options
 
 def setup(core, object):
+	#object.setAttachment('radial_filename', 'object/conversation');
+	#object.setAttachment('conversationFile','imp_recruiter')
+	object.setOptionsBitmask(Options.INVULNERABLE)
+	object.setStfFilename('mob/creature_names')
+	object.setStfName('tatooine_opening_niko')
 	return
