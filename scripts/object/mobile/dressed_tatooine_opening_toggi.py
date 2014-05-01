7043f6a15968714e2e27df07e0af1c75289910fa
tacef <neuwirth.christoph@gmx.net>
2014-05-01 19:38:12 +0200

    Updated NPC Scripts with conversations

--- scripts/object/mobile/dressed_tatooine_opening_toggi.py
+++ scripts/object/mobile/dressed_tatooine_opening_toggi.py
@@ -2,9 +2,9 @@ import sys
 from resources.datatables import Options
 
 def setup(core, object):
-	#object.setAttachment('radial_filename', 'object/conversation');
-	#object.setAttachment('conversationFile','imp_recruiter')
-	object.setOptionsBitmask(Options.INVULNERABLE)
+	object.setAttachment('radial_filename', 'object/conversation');
+	object.setAttachment('conversationFile','toggi')
+	object.setOptionsBitmask(Options.CONVERSABLE | Options.INVULNERABLE)
 	object.setStfFilename('mob/creature_names')
 	object.setStfName('tatooine_opening_toggi')
 	return
