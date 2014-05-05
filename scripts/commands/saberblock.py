b532798a0c7994a9d2a231455f6226e6b2d03353
zingzing175 <jeremy@zingstecharchive.com>
2014-05-04 18:01:57 -0700

    Fixed some commands not firing unless targeting self

--- scripts/commands/saberblock.py
+++ scripts/commands/saberblock.py
@@ -4,6 +4,6 @@ def setup(core, actor, buff):
 	return
 	
 def run(core, actor, target, commandString):
-	core.buffService.addBuffToCreature(actor, 'saberblock')
+	core.buffService.addBuffToCreature(actor, 'saberblock', actor)
 	return
 	
