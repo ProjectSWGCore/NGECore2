5f23d6b71638bff2e4129c07dac90c520fb2154a
Seefo <rezen1911@gmail.com>
2014-05-06 13:51:16 -0400

    Fixed some of the color crystal colors - they should be done now!

--- src/resources/datatables/LightsaberColors.java
+++ src/resources/datatables/LightsaberColors.java
@@ -30,33 +30,33 @@ public final class LightsaberColors {
 
 	static
 	{
-		// I put in the names of the really generic ones... I should be right up until Dark Purple... I'm unsure after Dark Purple.
-		map.put("Red", 0);
-		map.put("Dark Red", 1);
-		map.put("Light Green", 2);
-		map.put("Dark Green", 3);
-		map.put("Blue", 4);
-		map.put("Dark Blue", 5);
-		map.put("Yellow", 6);
-		map.put("Dark Yellow", 7);
-		map.put("Light Purple", 8);
-		map.put("Purple", 9);
+		// Basic color crystals with the basic lightsaber shader
+		map.put("Red", 0x00);
+		map.put("Dark Red", 0x01);
+		map.put("Light Green", 0x02);
+		map.put("Dark Green", 0x03);
+		map.put("Blue", 0x04);
+		map.put("Dark Blue", 0x05);
+		map.put("Yellow", 0x06);
+		map.put("Dark Yellow", 0x07);
+		map.put("Light Purple", 0x08);
+		map.put("Purple", 0x09);
 		map.put("Orange", 0x0A);
-		map.put("Browns", 0x0B);
+		map.put("Brown", 0x0B);
 		map.put("Maul's Vengeance", 0x0C);
 		map.put("Kun's Blood", 0x0D);
 		map.put("Bondara's Folly", 0x0E);
 		map.put("Ulic's Redemption", 0x0F);
-		map.put("Bane's Heart", 10);
-		map.put("Gallia's Intuition", 11);		
-		map.put("Prowess of Plo Koon", 12);
-		map.put("B'Nar's Sacrifice", 13);
-		map.put("Windu's Guile", 14);
-		map.put("Mundi's Response", 15);
-		map.put("Strength of Luminaria", 16);		
-		map.put("Baaa's Wisdom", 17);
-		map.put("Quintessence of Force", 18);
-		map.put("Dawn of Dagobah", 19);
+		map.put("Bane's Heart", 0x10);
+		map.put("Gallia's Intuition", 0x11);		
+		map.put("Prowess of Plo Koon", 0x12);
+		map.put("B'Nar's Sacrifice", 0x13);
+		map.put("Windu's Guile", 0x14);
+		map.put("Mundi's Response", 0x15);
+		map.put("Strength of Luminaria", 0x16);		
+		map.put("Baaa's Wisdom", 0x17);
+		map.put("Quintessence of Force", 0x18);
+		map.put("Dawn of Dagobah", 0x19);
 		map.put("Horn's Future", 0x1A);
 		map.put("Qui-Gon's Devotion", 0x1B);
 		map.put("Kenobi's Legacy", 0x1C);
