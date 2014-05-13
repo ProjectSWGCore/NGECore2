b66a518a557abe5964baab5164446ea5c2a40b0d
tacef <neuwirth.christoph@gmx.net>
2014-05-13 18:04:31 +0200

    Added more NPC scripts for Tatooine

---
+++ scripts/mobiles/tatooine/bomarr_monk_abbot.py
@@ -0,0 +1,37 @@
+import sys
+from services.spawn import MobileTemplate
+from services.spawn import WeaponTemplate
+from java.util import Vector
+
+def addTemplate(core):
+	mobileTemplate = MobileTemplate()
+	
+	mobileTemplate.setCreatureName('bomarr_monk_abbot')
+	mobileTemplate.setLevel(2)
+	mobileTemplate.setDifficulty(0)
+	mobileTemplate.setAttackRange(5)
+	mobileTemplate.setAttackSpeed(1.0)
+	mobileTemplate.setWeaponType(6)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(False)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setSocialGroup("spiderdroid")
+	mobileTemplate.setAssistRange(0)
+	mobileTemplate.setStalker(False)
+	
+	templates = Vector()
+	templates.add('object/mobile/shared_bomarr_spider_monk_droid.iff')
+	mobileTemplate.setTemplates(templates)
+	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/melee/unarmed/shared_unarmed_default.iff', 6, 1.0)
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
+	
+	attacks = Vector()
+	mobileTemplate.setDefaultAttack('creatureMeleeAttack')
+	mobileTemplate.setAttacks(attacks)
+	
+	core.spawnService.addMobileTemplate('bomarr_monk_abbot', mobileTemplate)
+	return
