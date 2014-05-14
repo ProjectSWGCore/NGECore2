37f20d56b65f8369fd08f512e5f7239631c78432
tacef <neuwirth.christoph@gmx.net>
2014-05-14 08:00:48 +0200

    Continued work on Tatooine NPC scripts

---
+++ scripts/mobiles/tatooine/fighting_romo_vax.py
@@ -0,0 +1,37 @@
+import sys
+from services.spawn import MobileTemplate
+from services.spawn import WeaponTemplate
+from java.util import Vector
+
+def addTemplate(core):
+	mobileTemplate = MobileTemplate()
+	
+	mobileTemplate.setCreatureName('fighting_romo_vax')
+	mobileTemplate.setLevel(20)
+	mobileTemplate.setDifficulty(0)
+	mobileTemplate.setAttackRange(15)
+	mobileTemplate.setAttackSpeed(1.0)
+	mobileTemplate.setWeaponType(1)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(False)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setSocialGroup("sennex")
+	mobileTemplate.setAssistRange(10)
+	mobileTemplate.setStalker(False)
+	
+	templates = Vector()
+	templates.add('object/mobile/shared_dressed_trader_thug_male_human_01.iff')
+	mobileTemplate.setTemplates(templates)
+	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/ranged/carbine/shared_carbine_e5.iff', 1, 1.0)
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
+	
+	attacks = Vector()
+	mobileTemplate.setDefaultAttack('rangedshot')
+	mobileTemplate.setAttacks(attacks)
+	
+	core.spawnService.addMobileTemplate('fighting_romo_vax', mobileTemplate)
+	return
