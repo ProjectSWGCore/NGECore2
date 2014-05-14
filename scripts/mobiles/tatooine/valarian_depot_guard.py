b251e815121f42205af5153b285067c16b75f7fd
tacef <neuwirth.christoph@gmx.net>
2014-05-14 17:21:24 +0200

    Added more Tatooine NPC scripts

---
+++ scripts/mobiles/tatooine/valarian_depot_guard.py
@@ -0,0 +1,38 @@
+import sys
+from services.spawn import MobileTemplate
+from services.spawn import WeaponTemplate
+from java.util import Vector
+
+def addTemplate(core):
+	mobileTemplate = MobileTemplate()
+	
+	mobileTemplate.setCreatureName('valarian_boss_shuttle')
+	mobileTemplate.setLevel(14)
+	mobileTemplate.setDifficulty(0)
+	mobileTemplate.setAttackRange(12)
+	mobileTemplate.setAttackSpeed(1.0)
+	mobileTemplate.setWeaponType(0)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(False)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setSocialGroup("valarian")
+	mobileTemplate.setAssistRange(4)
+	mobileTemplate.setStalker(False)
+	
+	
+	templates = Vector()
+	templates.add('object/mobile/shared_dressed_tatooine_valarian_thug.iff')
+	mobileTemplate.setTemplates(templates)
+	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/ranged/pistol/shared_pistol_cdef.iff', 2, 1.0)
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
+	
+	attacks = Vector()
+	mobileTemplate.setDefaultAttack('rangedshotpistol')
+	mobileTemplate.setAttacks(attacks)
+	
+	core.spawnService.addMobileTemplate('valarian_depot_guard', mobileTemplate)
+	return
