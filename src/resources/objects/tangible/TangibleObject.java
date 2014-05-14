fa4816921ed6379c396fbfcd09486195d2a9d099
Treeku <treeku@gmx.com>
2014-05-14 17:11:22 +0100

    Refactored GUILD object

--- src/resources/objects/tangible/TangibleObject.java
+++ src/resources/objects/tangible/TangibleObject.java
@@ -85,12 +85,14 @@ public class TangibleObject extends SWGObject implements Serializable {
 		super(objectID, planet, position, orientation, template);
 		messageBuilder = new TangibleMessageBuilder(this);
 		if (this.getClass().getSimpleName().equals("TangibleObject")) setIntAttribute("volume", 1);
+		otherVariables = getOtherVariables(); // FIXME temp
 	}
 	
 	public TangibleObject(long objectID, Planet planet, String template) {
 		super(objectID, planet, new Point3D(0, 0, 0), new Quaternion(1, 0, 1, 0), template);
 		messageBuilder = new TangibleMessageBuilder(this);
 		if (this.getClass().getSimpleName().equals("TangibleObject")) setIntAttribute("volume", 1);
+		otherVariables = getOtherVariables(); // FIXME temp
 	}
 	
 	public TangibleObject() {
