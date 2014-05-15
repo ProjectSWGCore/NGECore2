edc87fddc5fb2fb9c17b46d4fb45aa1a067be1b9
tacef <neuwirth.christoph@gmx.net>
2014-05-15 13:34:03 +0200

    Updated Spawns around eisley
    Reduced creetle spawns
    Fixed Mites floating above ground
    Added Swoopgang to Eisley

--- scripts/static_spawns/tatooine/mos_eisley_kreetles.py
+++ scripts/static_spawns/tatooine/mos_eisley_kreetles.py
@@ -21,9 +21,6 @@ def addPlanetSpawns(core, planet):
 	carrion_kreetle14 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3555), float(4), float(-5072), float(0), float(0), float(0), float(0), 30)	
 	carrion_kreetle15 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3559), float(4), float(-5061), float(0), float(0), float(0), float(0), 30)	
 	carrion_kreetle16 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3562), float(4), float(-5064), float(0), float(0), float(0), float(0), 30)	
-	carrion_kreetle17 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3565), float(4), float(-5058), float(0), float(0), float(0), float(0), 30)	
-	carrion_kreetle18 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3535), float(4), float(-5061), float(0), float(0), float(0), float(0), 30)	
-	carrion_kreetle19 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3551), float(4), float(-5058), float(0), float(0), float(0), float(0), 30)		
 	
 	carrion_kreetle2 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3233), float(8), float(-4457), float(0), float(0), float(0), float(0), 30)	
 	carrion_kreetle21 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3223), float(8), float(-4447), float(0), float(0), float(0), float(0), 30)	
@@ -32,19 +29,13 @@ def addPlanetSpawns(core, planet):
 	carrion_kreetle24 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3245), float(8), float(-4460), float(0), float(0), float(0), float(0), 30)	
 	carrion_kreetle25 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3250), float(8), float(-4461), float(0), float(0), float(0), float(0), 30)	
 	carrion_kreetle26 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3238), float(8), float(-4464), float(0), float(0), float(0), float(0), 30)	
-	carrion_kreetle27 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3228), float(8), float(-4470), float(0), float(0), float(0), float(0), 30)	
-	carrion_kreetle28 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3222), float(8), float(-4471), float(0), float(0), float(0), float(0), 30)	
-	carrion_kreetle29 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3239), float(8), float(-4437), float(0), float(0), float(0), float(0), 30)		
 		
 	carrion_kreetle3 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3147), float(4), float(-4846), float(0), float(0), float(0), float(0), 30)	
 	carrion_kreetle31 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3137), float(4), float(-4856), float(0), float(0), float(0), float(0), 30)	
 	carrion_kreetle32 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3127), float(4), float(-4836), float(0), float(0), float(0), float(0), 30)
-	carrion_kreetle33 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3157), float(4), float(-4840), float(0), float(0), float(0), float(0), 30)
-	carrion_kreetle34 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3167), float(4), float(-4850), float(0), float(0), float(0), float(0), 30)
 	carrion_kreetle35 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3145), float(4), float(-4852), float(0), float(0), float(0), float(0), 30)
 	carrion_kreetle36 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3135), float(4), float(-4838), float(0), float(0), float(0), float(0), 30)
 	carrion_kreetle37 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3150), float(4), float(-4856), float(0), float(0), float(0), float(0), 30)
-	carrion_kreetle38 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3163), float(4), float(-4844), float(0), float(0), float(0), float(0), 30)
 	carrion_kreetle39 = stcSvc.spawnObject('carrion_kreetle', 'tatooine', long(0), float(3140), float(4), float(-4860), float(0), float(0), float(0), float(0), 30)
 			
 	return
