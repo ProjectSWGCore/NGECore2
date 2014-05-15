20ebad914a150c259278659a5932ab508c2f4121
Tipho <nwesley130@hotmail.com>
2014-05-15 14:31:26 -0400

    Updated naboo creatures

--- scripts/mobiles/naboo/chuba.py
+++ scripts/mobiles/naboo/chuba.py
@@ -5,6 +5,10 @@ from java.util import Vector
 def addTemplate(core):
 	mobileTemplate = MobileTemplate()
 	templates = Vector()
+	add.attack('bm_bite_1')
+	add.attack('bm_spit_1')
+	add.attack('bm_damage_poison_1')
+	mobileTemplates.setAttacks(attacks)
 	templates.add('object/mobile/shared_chuba_hue.iff')
 	mobileTemplate.setCreatureName('chuba')
 	mobileTemplate.setTemplates(templates)
--- scripts/mobiles/naboo/narglatch.py
+++ scripts/mobiles/naboo/narglatch.py
@@ -5,6 +5,9 @@ from java.util import Vector
 def addTemplate(core):
 	mobileTemplate = MobileTemplate()
 	templates = Vector()
+	attacks.add('bm_claw_2')
+	attacks.add('bm_slash_2')
+	mobileTemplates.setAttacks(attacks)
 	templates.add('object/mobile/shared_narglatch_hue.iff')
 	mobileTemplate.setCreatureName('narglatch')
 	mobileTemplate.setTemplates(templates)
