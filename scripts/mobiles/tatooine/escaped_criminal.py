4f2a9e4abaa1541131cf445a63f51375cbe11d2f
tacef <neuwirth.christoph@gmx.net>
2014-05-16 21:59:44 +0200

    Updated some NPC scripts
    Updated some NPC scripts to reflect the latest changed to OPtionBitmask

--- scripts/mobiles/tatooine/escaped_criminal.py
+++ scripts/mobiles/tatooine/escaped_criminal.py
@@ -19,7 +19,7 @@ def addTemplate(core):
 	mobileTemplate.setSocialGroup("desert demon")
 	mobileTemplate.setAssistRange(4)
 	mobileTemplate.setStalker(False)
-	mobileTemplate.setOptionBitmask(192)
+	mobileTemplate.setOptionsBitmask(192)
 	
 	
 	templates = Vector()
