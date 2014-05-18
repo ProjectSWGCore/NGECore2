eddcf012022fd522fcb04fec1c9edccf930b4cf5
Treeku <treeku@gmx.com>
2014-05-18 19:07:13 +0100

    Implemented gcwPoints and gcwRanks

--- src/services/gcw/FactionService.java
+++ src/services/gcw/FactionService.java
@@ -47,7 +47,9 @@ import resources.objects.creature.CreatureObject;
 import resources.objects.mission.MissionObject;
 import resources.objects.player.PlayerObject;
 import resources.objects.tangible.TangibleObject;
+import engine.clientdata.ClientFileManager;
 import engine.clientdata.StfTable;
+import engine.clientdata.visitors.DatatableVisitor;
 import engine.clients.Client;
 import engine.resources.common.CRC;
 import engine.resources.service.INetworkDispatch;
@@ -352,28 +354,58 @@ public class FactionService implements INetworkDispatch {
 		return pvpBitmask;
 	}
 	
-	// temp fix until calculatePvpStatus is fixed
-	/*
-	public int calculatePvpStatus(CreatureObject player, TangibleObject target) {
-		
-		if(target.getSlottedObject("ghost") != null) {
-			
-			if(!player.getFaction().equals(target.getFaction()) && player.getFactionStatus() == FactionStatus.SpecialForces && ((CreatureObject) target).getFactionStatus() == FactionStatus.SpecialForces)
-				return 55;
-			else if(core.combatService.areInDuel(player, (CreatureObject) target))
-				return 55;
-			else
-				return 0x14;
-						
-		} else {
-			if(target.getAttachment("AI") != null)
-				return PvpStatus.Attackable;
-			else
-				return 0;
+	public boolean isPvpFaction(String faction) {
+		if (!isFaction(faction)) {
+			return false;
 		}
 		
+		try {
+			DatatableVisitor PvpFactions = ClientFileManager.loadFile("datatables/player/pvp_factions.iff", DatatableVisitor.class);
+			
+			for (int i = 0; i < PvpFactions.getRowCount(); i++) {
+				if (PvpFactions.getObject(i, 0) != null) {
+					if (((String) PvpFactions.getObject(i, 0)).equals(faction)) {
+						return true;
+					}
+				}
+			}
+			
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
 	}
-	*/
+	
+	public void join(CreatureObject actor, String faction) {
+		if (actor.getPvpBitmask() == PvpStatus.GoingCovert) {
+			actor.sendSystemMessage("@faction_recruiter:pvp_status_changing", DisplayType.Broadcast);
+			return;
+		}
+		
+		if (actor.getPvpBitmask() == PvpStatus.GoingOvert) {
+			actor.sendSystemMessage("@faction_recruiter:pvp_status_changing", DisplayType.Broadcast);
+			return;
+		}
+		
+		if (actor.getFaction().length() > 0) {
+			actor.sendSystemMessage("@faction_recruiter:resign_on_leave", DisplayType.Broadcast);
+			return;
+		}
+		
+		scheduler.schedule(() -> {
+			actor.setFaction(faction);
+			actor.setFactionStatus(FactionStatus.Combatant);
+			actor.setPvpBitmask(0);
+			PlayerObject player = ((PlayerObject) actor.getSlottedObject("ghost"));
+			if (player != null) {
+				player.resetGcwPoints();
+				player.resetPvpKills();
+				player.setCurrentRank(1);
+			}
+			actor.updatePvpStatus();
+		}, 1, TimeUnit.SECONDS);
+	}
 	
 	public void changeFactionStatus(CreatureObject actor, int factionStatus) {
 		long time = 1;
@@ -464,6 +496,12 @@ public class FactionService implements INetworkDispatch {
 			actor.setPvpBitmask(0);
 			actor.setFactionStatus(FactionStatus.OnLeave);
 			actor.setFaction("");
+			PlayerObject player = ((PlayerObject) actor.getSlottedObject("ghost"));
+			if (player != null) {
+				player.resetGcwPoints();
+				player.resetPvpKills();
+				player.setCurrentRank(0);
+			}
 			actor.updatePvpStatus();
 			actor.sendSystemMessage("@faction_recruiter:resign_complete", DisplayType.Broadcast);
 		}, time, TimeUnit.SECONDS);
