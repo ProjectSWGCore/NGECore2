54b93d4b190464383a007e416894e24b4afdeba8
Light2 <light_pswg@yahoo.com>
2014-05-21 20:35:23 +0200

    Changed krayt spawns to static spawns like on live

--- scripts/mobiles/spawnareas/krayt_graveyard_dynamic_1.py
+++ scripts/mobiles/spawnareas/krayt_graveyard_dynamic_1.py
@@ -7,5 +7,5 @@ def addSpawnArea(core):
 	dynamicGroups.add('juvenile_krayts_wild_1')
 	dynamicGroups.add('canyon_krayt_wild_1')
 	dynamicGroups.add('giant_canyon_krayt_wild_1')
-	core.spawnService.addDynamicSpawnArea(dynamicGroups, 6471, 4267, 1024, 'tatooine')
+	#core.spawnService.addDynamicSpawnArea(dynamicGroups, 6471, 4267, 1024, 'tatooine')
 	return
--- scripts/mobiles/spawnareas/krayt_graveyard_dynamic_2.py
+++ scripts/mobiles/spawnareas/krayt_graveyard_dynamic_2.py
@@ -4,5 +4,5 @@ from java.util import Vector
 def addSpawnArea(core):
 	dynamicGroups = Vector()
 	dynamicGroups.add('grand_canyon_krayt_wild_1')
-	core.spawnService.addDynamicSpawnArea(dynamicGroups, 7431, 4762, 120, 'tatooine')
+	#core.spawnService.addDynamicSpawnArea(dynamicGroups, 7431, 4762, 120, 'tatooine')
 	return
