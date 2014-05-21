33ddb83207d24ffa23a2ea9a26259059e30c33b8
Seefo <rezen1911@gmail.com>
2014-05-21 19:15:09 -0400

    Fixed #690

--- src/services/collections/CollectionService.java
+++ src/services/collections/CollectionService.java
@@ -38,6 +38,7 @@ import main.NGECore;
 import engine.clientdata.ClientFileManager;
 import engine.clientdata.StfTable;
 import engine.clientdata.visitors.DatatableVisitor;
+import engine.resources.common.Stf;
 import engine.resources.scene.Point3D;
 import engine.resources.service.INetworkDispatch;
 import engine.resources.service.INetworkRemoteEvent;
@@ -310,16 +311,16 @@ public class CollectionService implements INetworkDispatch {
 							}
 							
 							if (hidden && !(getCollection(creature, collectionName) > 0)) {
-								creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_hidden_slot_added", "TO", "@collection_n:" + collectionName), DisplayType.Broadcast);
+								creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_hidden_slot_added", "TO", new Stf("@collection_n:" + collectionName).getStfValue().replace(":", " -")), DisplayType.Broadcast);
 							}
 							
 							player.setCollections(collections.toByteArray());
 							
 							if (!noMessage) {
 								if (!bookName.equals("crafting_book")) {
-									creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_slot_added", "TU", "@collection_n:" + slotName, "TO", "@collection_n:" + collectionName), DisplayType.Broadcast);
+									creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_slot_added", "TU", new Stf("@collection_n:" + slotName).getStfValue().replace(":", " -"), "TO", new Stf("@collection_n:" + collectionName).getStfValue()), DisplayType.Broadcast);
 								} else {
-									creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_slot_increment", "TU", "@collection_n:" + slotName, "TO", "@collection_n:" + collectionName), DisplayType.Broadcast);
+									creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_slot_increment", "TU", new Stf("@collection_n:" + slotName).getStfValue().replace(":", " -"), "TO", new Stf("@collection_n:" + collectionName).getStfValue()), DisplayType.Broadcast);
 								}
 							}
 							
@@ -356,7 +357,7 @@ public class CollectionService implements INetworkDispatch {
 							}
 							
 							if (isComplete(creature, collectionName)) {
-								creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_collection_complete", "TO", "@collection_n:" + collectionName), DisplayType.Broadcast);
+								creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_collection_complete", "TO", new Stf("@collection_n:" + collectionName).getStfValue().replace(":", " -")), DisplayType.Broadcast);
 								
 								if (!noReward) {
 									try {
@@ -445,7 +446,7 @@ public class CollectionService implements INetworkDispatch {
 								if (trackServerFirst) {
 									if (core.guildService.getGuildObject().addServerFirst(collectionName, new ServerFirst(creature.getCustomName(), creature.getObjectId(), collectionName, System.currentTimeMillis()))) {
 										addCollection(creature, "bdg_server_first_01");
-										creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_server_first", "TT", core.getGalaxyName(), "TO", "@collection_n:" + collectionName), DisplayType.Broadcast);
+										creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_server_first", "TT", core.getGalaxyName(), "TO", new Stf("@collection_n:" + collectionName).getStfValue().replace(":", " -")), DisplayType.Broadcast);
 									}
 								}
 								
@@ -818,7 +819,7 @@ public class CollectionService implements INetworkDispatch {
 								}
 								
 								if (collection.equals(collectionName)) {
-									creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_collection_reset", "TO", "@collection_n:" + collectionName), DisplayType.Broadcast);
+									creature.sendSystemMessage(OutOfBand.ProsePackage("@collection:player_collection_reset", "TO", new Stf("@collection_n:" + collectionName).getStfValue().replace(":", " -")), DisplayType.Broadcast);
 								}
 								
 								continue;
