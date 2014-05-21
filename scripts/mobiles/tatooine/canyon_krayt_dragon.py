88bb8a4ee2663f7ae4fbaab406aa4f800b3eabfa
Light2 <light_pswg@yahoo.com>
2014-05-22 00:35:11 +0200

    fixed krayt sizes to more realistic values

--- scripts/mobiles/tatooine/canyon_krayt_dragon.py
+++ scripts/mobiles/tatooine/canyon_krayt_dragon.py
@@ -17,7 +17,7 @@ def addTemplate(core):
 	mobileTemplate.setMinSpawnDistance(4)
 	mobileTemplate.setMaxSpawnDistance(8)
 	mobileTemplate.setDeathblow(True)
-	mobileTemplate.setScale(0.8)
+	mobileTemplate.setScale(0.9)
 	mobileTemplate.setMeatType("Carnivore Meat")
 	mobileTemplate.setMeatAmount(1250)
 	mobileTemplate.setHideType("Bristly Hide")
