56edd2f5821371ba9762d3ebc6036a9535d32c06
Waverunner <Waverunner@users.noreply.github.com>
2014-05-24 20:15:50 -0400

    Fix error

--- scripts/mobiles/dathomir/bane_back_spider.py
+++ scripts/mobiles/dathomir/bane_back_spider.py
@@ -6,7 +6,8 @@ from java.util import Vector
 def addTemplate(core):
 	mobileTemplate = MobileTemplate()
 		
-	mobileTemplate.setCreatureName('bio_engineered_bane_back')		mobileTemplate.setLevel(80)
+	mobileTemplate.setCreatureName('bio_engineered_bane_back')		
+	mobileTemplate.setLevel(80)
 	mobileTemplate.setDifficulty(0)
 	mobileTemplate.setAttackRange(5)
 	mobileTemplate.setAttackSpeed(1.0)
