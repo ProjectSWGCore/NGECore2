7e30af8a2d08164c57ce61bd7b50c60991256dea
tacef <neuwirth.christoph@gmx.net>
2014-05-25 14:52:30 +0200

    Added Creature NPC Scripts to corellia

---
+++ scripts/mobiles/corellia/corellian_butterfly_worker.py
@@ -0,0 +1,42 @@
+import sys
+from services.spawn import MobileTemplate
+from services.spawn import WeaponTemplate
+from java.util import Vector
+
+def addTemplate(core):
+	mobileTemplate = MobileTemplate()
+	
+	mobileTemplate.setCreatureName('corellian_butterfly_warrior')
+	mobileTemplate.setLevel(28)
+	mobileTemplate.setDifficulty(0)
+	mobileTemplate.setAttackRange(5)
+	mobileTemplate.setAttackSpeed(1.0)
+	mobileTemplate.setWeaponType(6)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(False)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setMeatType("Insect Meat")
+	mobileTemplate.setMeatAmount(8)
+	mobileTemplate.setHideType("Scaley Hide")
+	mobileTemplate.setHideAmount(8)
+	mobileTemplate.setSocialGroup("corellian butterfly")
+	mobileTemplate.setAssistRange(0)
+	mobileTemplate.setStalker(False)
+	mobileTemplate.setOptionsBitmask(192)
+	
+	templates = Vector()
+	templates.add('object/mobile/shared_corellian_butterfly.iff')
+	mobileTemplate.setTemplates(templates)
+	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/melee/unarmed/shared_unarmed_default.iff', 6, 1.0)
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
+	
+	attacks = Vector()
+	mobileTemplate.setDefaultAttack('creatureMeleeAttack')
+	mobileTemplate.setAttacks(attacks)
+	
+	core.spawnService.addMobileTemplate('corellian_butterfly_worker', mobileTemplate)
+	return
---
+++ scripts/mobiles/corellia/dire_cat.py
@@ -0,0 +1,43 @@
+import sys
+from services.spawn import MobileTemplate
+from services.spawn import WeaponTemplate
+from java.util import Vector
+
+def addTemplate(core):
+	mobileTemplate = MobileTemplate()
+	
+	mobileTemplate.setCreatureName('dire_cat')
+	mobileTemplate.setLevel(28)
+	mobileTemplate.setDifficulty(0)
+	mobileTemplate.setAttackRange(5)
+	mobileTemplate.setAttackSpeed(1.0)
+	mobileTemplate.setWeaponType(6)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(True)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setMeatType("Carnivore Meat")
+	mobileTemplate.setMeatAmount(65)
+	mobileTemplate.setHideType("Bristly Hide")
+	mobileTemplate.setHideAmount(35)
+	mobileTemplate.setBoneType("Mammal Bones")
+	mobileTemplate.setBoneAmount(30)
+	mobileTemplate.setSocialGroup("dire cat")
+	mobileTemplate.setAssistRange(6)
+	mobileTemplate.setStalker(False)
+	
+	templates = Vector()
+	templates.add('object/mobile/shared_dire_cat.iff')
+	mobileTemplate.setTemplates(templates)
+	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/melee/unarmed/shared_unarmed_default.iff', 6, 1.0)
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
+	
+	attacks = Vector()
+	mobileTemplate.setDefaultAttack('creatureMeleeAttack')
+	mobileTemplate.setAttacks(attacks)
+	
+	core.spawnService.addMobileTemplate('dire_cat', mobileTemplate)
+	return
