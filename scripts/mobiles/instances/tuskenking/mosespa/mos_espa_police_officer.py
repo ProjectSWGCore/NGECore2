4c2b0efff16e68f96204b9021d502ef1b0367dad
tacef <neuwirth.christoph@gmx.net>
2014-05-29 06:09:47 +0200

    Started work on Tuskenking Instance NPC scripts

---
+++ scripts/mobiles/instances/tuskenking/mosespa/mos_espa_police_officer.py
@@ -0,0 +1,39 @@
+import sys
+from services.spawn import MobileTemplate
+from services.spawn import WeaponTemplate
+from java.util import Vector
+
+def addTemplate(core):
+	mobileTemplate = MobileTemplate()
+	
+	mobileTemplate.setCreatureName('heroic_tusken_mos_espa_police_officer')
+	mobileTemplate.setLevel(90)
+	mobileTemplate.setDifficulty(1)
+	mobileTemplate.setAttackRange(12)
+	mobileTemplate.setAttackSpeed(1.0)
+	mobileTemplate.setWeaponType(2)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(True)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setSocialGroup("heroic mos espa citizien")
+	mobileTemplate.setAssistRange(12)
+	mobileTemplate.setStalker(True)
+	mobileTemplate.setOptionsBitmask(128)
+	
+	templates = Vector()
+	templates.add('object/mobile/shared_dressed_tatooine_mos_eisley_police_officer.iff')
+	mobileTemplate.setTemplates(templates)
+	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/ranged/pistol/shared_pistol_scout_blaster.iff', 2, 1.0)
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
+
+	attacks = Vector()
+	mobileTemplate.setDefaultAttack('rangedShot')
+	attacks.add('meleeHit')
+	mobileTemplate.setAttacks(attacks)
+	
+	core.spawnService.addMobileTemplate('heroic_mos_espa_police_officer', mobileTemplate)
+	return
