7e156d16db759b97182fda6643a8f10fce20fee7
Light2 <light_pswg@yahoo.com>
2014-05-31 21:01:14 +0200

    Added static gcw bases pvp zones

--- scripts/pvpzones.py
+++ scripts/pvpzones.py
@@ -5,5 +5,6 @@ def addZones(core):
 	gcwService = core.gcwService
 	
 	gcwService.addPvPZone('rori', 5318, 5680, 400)
-	
-	
+	gcwService.addPvPZone('corellia', 4722, -5233, 200)	
+	gcwService.addPvPZone('naboo', 1019, -1508, 150)	
+	gcwService.addPvPZone('talus', -4899, -3137, 150)		
