18318d8715eb6e562850ebf9792c585a6d11c040
tacef <neuwirth.christoph@gmx.net>
2014-06-01 14:19:55 +0200

    More Work on NPC scripts
    Added Junkdealers to Corellia( a lot Coords missing)
    Added Junkdealers to Naboo(a lot Coords missing)

---
+++ scripts/static_spawns/corellia/kor_vella.py
@@ -0,0 +1,12 @@
+
+import sys
+from resources.datatables import Options
+from resources.datatables import StateStatus
+
+
+def addPlanetSpawns(core, planet):
+
+
+	#junkdealer will be added as soon as i find coords
+
+	return	
---
+++ scripts/static_spawns/naboo/keren.py
@@ -0,0 +1,16 @@
+import sys
+from resources.datatables import Options
+from resources.datatables import StateStatus
+
+
+def addPlanetSpawns(core, planet):
+
+	stcSvc = core.staticService
+	objSvc = core.objectService
+	
+	#junkdealer 
+	stcSvc.spawnObject('object/mobile/shared_junk_dealer_m_01.iff', 'naboo', long(0), float(1242), float(13), float(2732), float(0.707), float(0.707))
+	stcSvc.spawnObject('object/mobile/shared_junk_dealer_m_01.iff', 'naboo', long(0), float(1774), float(12.5), float(2625), float(0.707), float(0.707))
+	
+	
+	return	
