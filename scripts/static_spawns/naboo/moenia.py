9ac74b48c5b473d55e63939e5df7ce0ee919a54a
tacef <neuwirth.christoph@gmx.net>
2014-06-01 21:28:42 +0200

    More Work on Junkdealer Scripts
    Added some Junkdealers to moenia, mos espa und nyms stronghold.

--- scripts/static_spawns/naboo/moenia.py
+++ scripts/static_spawns/naboo/moenia.py
@@ -6,7 +6,12 @@ from resources.datatables import StateStatus
 
 def addPlanetSpawns(core, planet):
 
-
-	#junkdealer will be added as soon as i find coords
-
+	stcSvc = core.staticService
+	objSvc = core.objectService
+	
+	# TODO Check all NPCs for personalized scripting, change format.
+	
+	#Junkdealer
+	stcSvc.spawnObject('object/mobile/shared_junk_dealer_m_03.iff', 'naboo', long(0), float(4318.83), float(5.15), float(-4797.57), float(0), float(1))
+	
 	return	
