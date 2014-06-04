803fbba36a18efac09302090343647cf63ff8aeb
Zing <jeremy@zingstecharchive.com>
2014-06-03 23:32:32 -0700

    small fix

--- scripts/loot/lootItems/relics/jedi_relic_1.py
+++ scripts/loot/lootItems/relics/jedi_relic_1.py
@@ -2,10 +2,6 @@
 def itemTemplate():
 
 	return ['object/tangible/loot/mustafar/shared_jedi_relic_03.iff']
-
-def customItemName():
-
-	return '@string/en/static_item_n:col_lightsaber_1h_01_03'
 	
 def usedInCollection():
 
