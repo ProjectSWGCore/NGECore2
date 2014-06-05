a4230a844bc8c9fd25e1366506c5ce9d820e6513
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 18:23:07 -0400

    Scripted Legendary Weapons

--- scripts/object/weapon/ranged/rifle/rifle_t21_legendary.py
+++ scripts/object/weapon/ranged/rifle/rifle_t21_legendary.py
@@ -1,4 +1,11 @@
 import sys
 
 def setup(core, object):
+	object.setIntAttribute('required_combat_level', 88)
+	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 0.8)
+	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
+	object.setStringAttribute('cat_wpn_damage.damage', '550-1100')
+	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-64m')
+	object.setStringAttribute('cat_wpn_damage.wpn_category', 'Rifle')
+	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
 	return
