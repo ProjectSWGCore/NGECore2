4ae300aa556eaa205de826bd073b76e20e4da48c
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 08:41:56 -0400

    Revert "Rucksack of the Tusken King"
    This reverts commit a9ac0c425d1daa5233af537fa1b29e37abcae3d3.

--- scripts/object/tangible/wearables/backpack/backpack_krayt_skull.py
+++ scripts/object/tangible/wearables/backpack/backpack_krayt_skull.py
@@ -2,25 +2,3 @@ import sys
 
 def setup(core, object):
 	return
-	
-def equip(core, actor, target):
-	core.skillModService.addSkillMod(actor, 'agility_modified', 25)
-	core.skillModService.addSkillMod(actor, 'constitution_modified', 30)
-	core.skillModService.addSkillMod(actor, 'luck_modified', 25)
-	core.skillModService.addSkillMod(actor, 'precision_modified', 35)
-	core.skillModService.addSkillMod(actor, 'stamina_modified', 30)
-	core.skillModService.addSkillMod(actor, 'strength_modified', 35)
-	
-	
-	return
-	
-def unequip(core, actor, target):
-	core.skillModService.deductSkillMod(actor, 'agility_modified', 25)
-	core.skillModService.deductSkillMod(actor, 'constitution_modified', 30)
-	core.skillModService.deductSkillMod(actor, 'luck_modified', 25)
-    core.skillModService.deductSkillMod(actor, 'precision_modified', 35)
-    core.skillModService.deductSkillMod(actor, 'stamina_modified', 30)
-    core.skillModService.deductSkillMod(actor, 'strength_modified', 35)
-
-	return
-	
