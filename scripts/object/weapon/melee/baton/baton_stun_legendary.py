617ac198b51de12a17cf067ccdc5795c9c098e07
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 19:24:24 -0400

    Revert "Changes to Legendary Weapons"
    This reverts commit 9004f6a816083ed101866dec08e853171705f7ad.

--- scripts/object/weapon/melee/baton/baton_stun_legendary.py
+++ scripts/object/weapon/melee/baton/baton_stun_legendary.py
@@ -1,11 +1,11 @@
 import sys
 
 def setup(core, object):
-	object.setIntAttribute('required_combat_level', 88)	
-	object.setAttackSpeed(1);
-	object.setMaxRange(5);
-	object.setDamageType("energy");
-	object.setMinDamage(600);
-	object.setMaxDamage(1300);
-	object.setWeaponType(4);
+	object.setIntAttribute('required_combat_level', 88)
+	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 1)
+	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
+	object.setStringAttribute('cat_wpn_damage.damage', '600-1300')
+	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-5m')
+	object.setStringAttribute('cat_wpn_damage.wpn_category', 'Baton')
+	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
 	return
