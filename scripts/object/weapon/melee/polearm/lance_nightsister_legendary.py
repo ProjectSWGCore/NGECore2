5cd8a835cd5bdff7f022a0ba1e1d7ddfb5a5674d
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 19:24:24 -0400

    Revert "Changes to Legendary Weapons"
    This reverts commit 9004f6a816083ed101866dec08e853171705f7ad.

--- scripts/object/weapon/melee/polearm/lance_nightsister_legendary.py
+++ scripts/object/weapon/melee/polearm/lance_nightsister_legendary.py
@@ -1,11 +1,11 @@
 import sys
 
 def setup(core, object):
-	object.setIntAttribute('required_combat_level', 88)	
-	object.setAttackSpeed(1);
-	object.setMaxRange(5);
-	object.setDamageType("energy");
-	object.setMinDamage(800);
-	object.setMaxDamage(1200);
-	object.setWeaponType(7);
+	object.setIntAttribute('required_combat_level', 88)
+	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 1)
+	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
+	object.setStringAttribute('cat_wpn_damage.damage', '800-1200')
+	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-5m')
+	object.setStringAttribute('cat_wpn_damage.wpn_category', 'Polearm')
+	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
 	return
