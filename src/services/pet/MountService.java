7dcfdca02ccbe96f37cf4acdf5ffa9bc32ba4884
Treeku <treeku@gmx.com>
2014-06-06 00:16:52 +0100

    Fixed mounts not being storable while on them

--- src/services/pet/MountService.java
+++ src/services/pet/MountService.java
@@ -674,7 +674,7 @@ public class MountService implements INetworkDispatch {
 		}
 		
 		if (isMounted(owner, mount)) {
-			storer.sendSystemMessage(OutOfBand.ProsePackage("@pet/pet_menu:must_dismount"), DisplayType.Broadcast);
+			//storer.sendSystemMessage(OutOfBand.ProsePackage("@pet/pet_menu:must_dismount"), DisplayType.Broadcast);
 			return;
 		}
 		
