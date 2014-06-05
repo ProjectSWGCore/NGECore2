e038c10abd7f4799ed3e9e13d53e7c1f4c8e8734
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 18:59:11 -0400

    Changes to Legendary Weapons

--- scripts/object/weapon/melee/2h_sword/2h_sword_maul_legendary.py
+++ scripts/object/weapon/melee/2h_sword/2h_sword_maul_legendary.py
@@ -1,11 +1,11 @@
 import sys
 
 def setup(core, object):
-	object.setIntAttribute('required_combat_level', 88)
-	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 1)
-	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
-	object.setStringAttribute('cat_wpn_damage.damage', '900-1350')
-	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-5m')
-	object.setStringAttribute('cat_wpn_damage.wpn_category', '2-Handed')
-	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
+	object.setIntAttribute('required_combat_level', 88)	
+	object.setAttackSpeed(1);
+	object.setMaxRange(5);
+	object.setDamageType("energy");
+	object.setMinDamage(900);
+	object.setMaxDamage(1350);
+	object.setWeaponType(5);
 	return
--- scripts/object/weapon/ranged/carbine/carbine_e5_legend.py
+++ scripts/object/weapon/ranged/carbine/carbine_e5_legend.py
@@ -1,11 +1,11 @@
 import sys
 
 def setup(core, object):
-	object.setIntAttribute('required_combat_level', 88)
-	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 0.6)
-	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
-	object.setStringAttribute('cat_wpn_damage.damage', '400-850')
-	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-50m')
-	object.setStringAttribute('cat_wpn_damage.wpn_category', 'Carbine')
-	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
+	object.setIntAttribute('required_combat_level', 88)	
+	object.setAttackSpeed(0.6);
+	object.setMaxRange(50);
+	object.setDamageType("energy");
+	object.setMinDamage(400);
+	object.setMaxDamage(850);
+	object.setWeaponType(1);
 	return
