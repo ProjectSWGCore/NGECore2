cbd1b83d7bb9a51aa4081db9cea5f209df6f8423
Levarrishawk <levarrishawk@gmail.com>
2014-06-05 06:00:46 -0400

    Added Missing Mos Eisley NPCs

--- scripts/static_spawns/tatooine/mos_eisley.py
+++ scripts/static_spawns/tatooine/mos_eisley.py
@@ -6,6 +6,7 @@ import sys
 
 from resources.datatables import Options
 from resources.datatables import State
+from resources.datatables import StateStatus
 
 def addPlanetSpawns(core, planet):
 
@@ -13,7 +14,7 @@ def addPlanetSpawns(core, planet):
 	stcSvc = core.staticService
 	objSvc = core.objectService
 	
-	# TODO Check all NPCs for personalized scripting, change format.
+	# TODO:  Add NPCs sitting at Cantina tables with SittingOnChair bitmask.
 	
 	#Cantina Interior
 	building = core.objectService.getObject(long(1082874)) 
@@ -105,7 +106,7 @@ def addPlanetSpawns(core, planet):
 	anetiaKahryn.setCustomName('Anetia Kah\'ryn')
 	anetiaKahryn.setOptionsBitmask(256)
 	
-	figrinDan = stcSvc.spawnObject('object/mobile/tatooine_npc/shared_figrin_dan.iff', 'tatooine', building.getCellByCellNumber(6), float(3.7), float(-0.9), float(-14.4), float(0), float(0.42), float(0), float(0.91)) 
+	figrinDan = stcSvc.spawnObject('object/mobile/shared_dressed_tatooine_figrin_dan.iff', 'tatooine', building.getCellByCellNumber(6), float(3.7), float(-0.9), float(-14.4), float(0), float(0.42), float(0), float(0.91)) 
 	figrinDan.setCustomName('Figrin D\'an')
 	figrinDan.setOptionsBitmask(256)
 	
@@ -142,6 +143,10 @@ def addPlanetSpawns(core, planet):
 	building = core.objectService.getObject(long(-466404040703447347)) 
 	stcSvc.spawnObject('object/mobile/shared_han_solo.iff', 'tatooine', building.getCellByCellNumber(9), float(32.3), float(7.0), float(1.6), float(0), float(0), float(0), float(0)) 
 	
+	valarian = stcSvc.spawnObject('object/mobile/shared_lady_valarian.iff', 'tatooine', building.getCellByCellNumber(4), float(-21.5), float(9.0), float(0.5), float(0), float(0.70), float(0), float(-0.71)) 
+	valarian.setCustomName('Lady Valarian')
+	valarian.setOptionsBitmask(256)
+	
 	#Medical Center Interior
 	
 	#Theater Interior
@@ -150,155 +155,165 @@ def addPlanetSpawns(core, planet):
 
 	#Outside
 	businessman2 = stcSvc.spawnObject('object/mobile/shared_dressed_businessman_human_male_01.iff', 'tatooine', long(0), float(3663.3), float(4.0), float(-4738.6), float(0), float(0)) 
-	businessman2.setCustomName2('a Businessman')
+	businessman2.setCustomName('a Businessman')
 	businessman2.setOptionsBitmask(256)
 	
 	noble2 = stcSvc.spawnObject('object/mobile/shared_dressed_noble_human_female_01.iff', 'tatooine', long(0), float(3542.3), float(5.0), float(-4826.0), float(0.42), float(0.91)) 
-	noble2.setCustomName2('a Noble')
+	noble2.setCustomName('a Noble')
 	noble2.setOptionsBitmask(256)
 	
 	commoner11 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_human_male_06.iff', 'tatooine', long(0), float(3529.1), float(5.0), float(-4900.4), float(0.42), float(0.91)) 
-	commoner11.setCustomName2('a Commoner')
+	commoner11.setCustomName('a Commoner')
 	commoner11.setOptionsBitmask(256)
 	
 	businessman3 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_tatooine_trandoshan_male_02.iff', 'tatooine', long(0), float(3595.7), float(5.0), float(-4740.1), float(0), float(0)) 
-	businessman3.setCustomName2('a Businessman')
+	businessman3.setCustomName('a Businessman')
 	businessman3.setOptionsBitmask(256)
 	
 	jawa1 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3663.3), float(5.0), float(-4858.6), float(0), float(0)) 
-	jawa1.setCustomName2('a Jawa')
+	jawa1.setCustomName('a Jawa')
 	jawa1.setOptionsBitmask(256)
 	
 	commoner12 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_moncal_male_01.iff', 'tatooine', long(0), float(3490.3), float(5.0), float(-4799.4), float(0), float(0)) 
-	commoner12.setCustomName2('a Scientist')
+	commoner12.setCustomName('a Scientist')
 	commoner12.setOptionsBitmask(256)
 	
 	commoner13 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_zabrak_female_02.iff', 'tatooine', long(0), float(3559.7), float(5.0), float(-4725.9), float(0), float(0)) 
-	commoner13.setCustomName2('a Commoner')
+	commoner13.setCustomName('a Commoner')
 	commoner13.setOptionsBitmask(256)
 	
 	commoner14 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_tatooine_devaronian_male_01.iff', 'tatooine', long(0), float(3527.7), float(5.0), float(-4721.1), float(0.71), float(0.71)) 
-	commoner14.setCustomName2('a Commoner')
+	commoner14.setCustomName('a Commoner')
 	commoner14.setOptionsBitmask(256)
 	
 	commoner15 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_tatooine_aqualish_female_01.iff', 'tatooine', long(0), float(3514.9), float(5.0), float(-4737.8), float(0), float(0)) 
-	commoner15.setCustomName2('a Commoner')
+	commoner15.setCustomName('a Commoner')
 	commoner15.setOptionsBitmask(256)
 	
 	jawa2 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3485.2), float(4.9), float(-4859.2), float(0), float(0)) 
-	jawa2.setCustomName2('a Jawa')
+	jawa2.setCustomName('a Jawa')
 	jawa2.setOptionsBitmask(256)
 	
 	jawa3 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3465.3), float(5.0), float(-4860.1), float(0.71), float(-0.71)) 
-	jawa3.setCustomName2('a Jawa')
+	jawa3.setCustomName('a Jawa')
 	jawa3.setOptionsBitmask(256)
 	
 	jawa4 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3486.8), float(5.0), float(-4884.7), float(0.43051), float(-0.9025)) 
-	jawa4.setCustomName2('a Jawa')
+	jawa4.setCustomName('a Jawa')
 	jawa4.setOptionsBitmask(256)
 	
 	jawa5 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3487.1), float(5.0), float(-4886.0), float(0.95105), float(0.3090)) 
-	jawa5.setCustomName2('a Jawa')
+	jawa5.setCustomName('a Jawa')
 	jawa5.setOptionsBitmask(256)
 	
 	jawa6 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3488.8), float(5.0), float(-4884.4), float(0.3255), float(-0.9455)) 
-	jawa6.setCustomName2('a Jawa')
+	jawa6.setCustomName('a Jawa')
 	jawa6.setOptionsBitmask(256)
 	
 	jawa7 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3472.2), float(5.0), float(-4918.4), float(0), float(0)) 
-	jawa7.setCustomName2('a Jawa')
+	jawa7.setCustomName('a Jawa')
 	jawa7.setOptionsBitmask(256)
 	
 	jawa8 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3471.4), float(5.0), float(-4919.5), float(0), float(0)) 
-	jawa8.setCustomName2('a Jawa')
+	jawa8.setCustomName('a Jawa')
 	jawa8.setOptionsBitmask(256)
 	
 	jawa9 = stcSvc.spawnObject('object/mobile/shared_jawa.iff', 'tatooine', long(0), float(3470.3), float(5.0), float(-4918.7), float(0), float(0)) 
-	jawa9.setCustomName2('a Jawa')
+	jawa9.setCustomName('a Jawa')
 	jawa9.setOptionsBitmask(256)
 	
 	bib = stcSvc.spawnObject('object/mobile/shared_bib_fortuna.iff', 'tatooine', long(0), float(3552.4), float(5.0), float(-4933.2), float(0.31730), float(-0.9483))
-	bib.setCustomName2('Bib Fortuna')
+	bib.setCustomName('Bib Fortuna')
 	bib.setOptionsBitmask(256)
 	
 	commoner16 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_human_male_06.iff', 'tatooine', long(0), float(3398.2), float(4.0), float(-4654.2), float(0.42), float(0.91)) 
-	commoner16.setCustomName2('a Commoner')
+	commoner16.setCustomName('a Commoner')
 	commoner16.setOptionsBitmask(256)
 	
 	noble3 = stcSvc.spawnObject('object/mobile/shared_dressed_noble_human_female_01.iff', 'tatooine', long(0), float(3396.3), float(4.0), float(-4774.1), float(0.42), float(0.91)) 
-	noble3.setCustomName2('a Noble')
+	noble3.setCustomName('a Noble')
 	noble3.setOptionsBitmask(256)
 	
 	entertainer1 = stcSvc.spawnObject('object/mobile/shared_dressed_entertainer_trainer_twk_female_01.iff', 'tatooine', long(0), float(3305.7), float(5.6), float(-4771.7), float(0), float(0)) 
-	entertainer1.setCustomName2('an Entertainer')
+	entertainer1.setCustomName('an Entertainer')
 	entertainer1.setOptionsBitmask(256)
 	
 	r3m6 = stcSvc.spawnObject('object/mobile/shared_r3.iff', 'tatooine', long(0), float(3460.1), float(4.0), float(-4898.2), float(0.38), float(-0.92)) 
-	r3m6.setCustomName2('R3-M6')
+	r3m6.setCustomName('R3-M6')
 	r3m6.setOptionsBitmask(256)
 	
 	eg1 = stcSvc.spawnObject('object/mobile/shared_eg6_power_droid.iff', 'tatooine', long(0), float(3463.8), float(4.0), float(-4882.6), float(-0.38), float(0.92)) 
-	eg1.setCustomName2('an EG-6 Power Droid')
+	eg1.setCustomName('an EG-6 Power Droid')
 	eg1.setOptionsBitmask(256)
 
 	commoner17 = stcSvc.spawnObject('object/mobile/shared_dressed_commoner_naboo_zabrak_female_02.iff', 'tatooine', long(0), float(3452.6), float(4.0), float(-4937.1), float(0), float(0)) 
-	commoner17.setCustomName2('a Commoner')
+	commoner17.setCustomName('a Commoner')
 	commoner17.setOptionsBitmask(256)
 	
 	lifter1 = stcSvc.spawnObject('object/mobile/shared_cll8_binary_load_lifter.iff', 'tatooine', long(0), float(3547), float(5.0), float(-4768.9), float(0), float(0)) 
-	lifter1.setCustomName2('a CLL-8 Binary Load Lifter')
+	lifter1.setCustomName('a CLL-8 Binary Load Lifter')
 	lifter1.setOptionsBitmask(256)
 	
 	r3j7 = stcSvc.spawnObject('object/mobile/shared_r3.iff', 'tatooine', long(0), float(3311.1), float(4.0), float(-4820.2), float(0.38), float(-0.92)) 
-	r3j7.setCustomName2('R3-J7')
+	r3j7.setCustomName('R3-J7')
 	r3j7.setOptionsBitmask(256)
 	
 	noble4 = stcSvc.spawnObject('object/mobile/shared_dressed_noble_human_female_03.iff', 'tatooine', long(0), float(3255.3), float(4.0), float(-4848.1), float(0.42), float(0.91)) 
-	noble4.setCustomName2('a Noble')
+	noble4.setCustomName('a Noble')
 	noble4.setOptionsBitmask(256)
 	
 	byxlePedette = stcSvc.spawnObject('object/mobile/shared_dressed_tatooine_opening_byxle.iff', 'tatooine', long(0), float(3365), float(5), float(-4639), float(0.99), float(0.12)) 
 	byxlePedette.setOptionsBitmask(264)
 	
 	errikDarksider = stcSvc.spawnObject('object/mobile/shared_dressed_herald_tatooine_01.iff', 'tatooine', long(0), float(3381), float(4.6), float(-4498), float(0.91), float(0.40)) 
-	errikDarksider.setCustomName2('Errik Darksider')
+	errikDarksider.setCustomName('Errik Darksider')
 	errikDarksider.setOptionsBitmask(256)
 	
 	gendra = stcSvc.spawnObject('object/mobile/shared_dressed_gendra.iff', 'tatooine', long(0), float(3308), float(5.6), float(-4785), float(0.84), float(0.53)) 
 	gendra.setOptionsBitmask(264)
 	
 	lurval = stcSvc.spawnObject('object/mobile/shared_lurval.iff', 'tatooine', long(0), float(3387), float(5), float(-4791), float(-0.4), float(0.91)) 
-	lurval.setCustomName2('Lurval')
+	lurval.setCustomName('Lurval')
 	lurval.setOptionsBitmask(256)
 	
 	matildaCarson = stcSvc.spawnObject('object/mobile/shared_dressed_noble_human_female_02.iff', 'tatooine', long(0), float(3490.2), float(5), float(-4778), float(0.87), float(0.48)) 
-	matildaCarson.setCustomName2('Matilda Carson')
+	matildaCarson.setCustomName('Matilda Carson')
 	matildaCarson.setOptionsBitmask(256)
 	
 	vanvi = stcSvc.spawnObject('object/mobile/shared_dressed_bestine_artist01.iff', 'tatooine', long(0), float(3312), float(5), float(-4655), float(0.95), float(-0.28)) 
-	vanvi.setCustomName2('Vanvi Hotn')
+	vanvi.setCustomName('Vanvi Hotn')
 	vanvi.setOptionsBitmask(256)
+	
+	harburik = stcSvc.spawnObject('object/mobile/shared_dressed_tatooine_lt_harburik.iff', 'tatooine', long(0), float(3486), float(5), float(-4740), float(0), float(-0.71), float(0), float(0.71)) 
+	harburik.setCustomName('Lt. Harburik')
+	harburik.setOptionsBitmask(256)
+	
+	trehla = stcSvc.spawnObject('object/mobile/shared_dressed_tutorial_mentor.iff', 'tatooine', long(0), float(3484), float(5), float(-4808), float(0), float(0.92), float(0), float(-0.38)) 
+	trehla.setCustomName('Trehla Keelo')
+	trehla.setOptionsBitmask(256)
 	
 	#Eisley Legacy Quest NPCs
 	
 	vourk = stcSvc.spawnObject('object/mobile/shared_dressed_tatooine_opening_santos.iff', 'tatooine', long(0), float(3520.0), float(5.0), float(-4821.0), float(0.42), float(0.91))
-	vourk.setCustomName2('Vourk Ver\'Zremp')
+	vourk.setCustomName('Vourk Ver\'Zremp')
 	vourk.setOptionsBitmask(256)
 	
-	mayor = stcSvc.spawnObject('object/mobile/shared_dressed_mayor_mikdanyell_guhrantt.iff', 'tatooine', long(1279960), float(1.2), float(2.5), float(5.4), float(0), float(0))
+	building = core.objectService.getObject(long(1279956)) 
+	mayor = stcSvc.spawnObject('object/mobile/shared_dressed_mayor_mikdanyell_guhrantt.iff', 'tatooine', building.getCellByCellNumber(4), float(1.2), float(2.5), float(5.4), float(0), float(0), float(0), float(0))
 	#mayor.setOptionsBitmask(264)
 	
 	enthaKandela = stcSvc.spawnObject('object/mobile/shared_dressed_entha_kandela.iff', 'tatooine', long(0), float(3511), float(5.0), float(-4785), float(0.70), float(0.71))
-	enthaKandela.setCustomName2('Entha Kandela')
+	enthaKandela.setCustomName('Entha Kandela')
 	enthaKandela.setOptionsBitmask(256)
 	
 	purvis = stcSvc.spawnObject('object/mobile/shared_dressed_purvis_arrison.iff', 'tatooine', long(0), float(3512.4), float(5.0), float(-4764.9), float(0.38), float(-0.92))
-	purvis.setCustomName2('Purvis Arrison')
+	purvis.setCustomName('Purvis Arrison')
 	purvis.setOptionsBitmask(256)
 	
-	peawpRdawc = stcSvc.spawnObject('object/mobile/shared_peawp_bodyguard_trainer.iff', 'tatooine', long(1189639), float(-11.8), float(1.1), float(-10.1), float(0), float(0))
-	#peawpRdawc.setCustomName2('Peawp R\'dawc')
-	#peawpRdawc.setOptionsBitmask(256)
+	building = core.objectService.getObject(long(1189630)) 
+	peawpRdawc = stcSvc.spawnObject('object/mobile/shared_peawp_bodyguard_trainer.iff', 'tatooine', building.getCellByCellNumber(9), float(-11.8), float(1.1), float(-10.1), float(0), float(0), float(0), float(0))
+	peawpRdawc.setCustomName('Peawp R\'dawc')
+	peawpRdawc.setOptionsBitmask(256)
 	
 	nikoBrehe = stcSvc.spawnObject('object/mobile/shared_dressed_tatooine_opening_niko.iff', 'tatooine', long(0), float(3506.7), float(5.0), float(-4795.8), float(0.70), float(0.71))
 	nikoBrehe.setOptionsBitmask(264)
