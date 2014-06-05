76968cbc064eb2bb58c711420a3bbc41abe05415
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 19:24:58 -0400

    Revert "Scripted Legendary Weapons"
    This reverts commit 5bd1576614181a674ec83b7d3f5ff7c6a987c5ac.

--- scripts/object/weapon/melee/2h_sword/2h_sword_maul_legendary.py
+++ scripts/object/weapon/melee/2h_sword/2h_sword_maul_legendary.py
@@ -1,11 +1,4 @@
 import sys
 
 def setup(core, object):
-	object.setIntAttribute('required_combat_level', 88)
-	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 1)
-	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
-	object.setStringAttribute('cat_wpn_damage.damage', '900-1350')
-	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-5m')
-	object.setStringAttribute('cat_wpn_damage.wpn_category', '2-Handed')
-	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
 	return
--- scripts/object/weapon/ranged/carbine/carbine_e5_legend.py
+++ scripts/object/weapon/ranged/carbine/carbine_e5_legend.py
@@ -1,11 +1,4 @@
 import sys
 
 def setup(core, object):
-	object.setIntAttribute('required_combat_level', 88)
-	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 0.6)
-	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
-	object.setStringAttribute('cat_wpn_damage.damage', '400-850')
-	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-50m')
-	object.setStringAttribute('cat_wpn_damage.wpn_category', 'Carbine')
-	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
 	return
