5bd1576614181a674ec83b7d3f5ff7c6a987c5ac
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 18:23:07 -0400

    Scripted Legendary Weapons

--- scripts/object/weapon/melee/polearm/lance_nightsister_legendary.py
+++ scripts/object/weapon/melee/polearm/lance_nightsister_legendary.py
@@ -1,4 +1,11 @@
 import sys
 
 def setup(core, object):
+	object.setIntAttribute('required_combat_level', 88)
+	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 1)
+	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
+	object.setStringAttribute('cat_wpn_damage.damage', '800-1200')
+	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-5m')
+	object.setStringAttribute('cat_wpn_damage.wpn_category', 'Polearm')
+	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
 	return
