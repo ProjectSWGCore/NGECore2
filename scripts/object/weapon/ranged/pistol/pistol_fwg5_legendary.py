9004f6a816083ed101866dec08e853171705f7ad
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-05 18:59:11 -0400

    Changes to Legendary Weapons

--- scripts/object/weapon/ranged/pistol/pistol_fwg5_legendary.py
+++ scripts/object/weapon/ranged/pistol/pistol_fwg5_legendary.py
@@ -1,11 +1,11 @@
 import sys
 
 def setup(core, object):
-	object.setIntAttribute('required_combat_level', 88)
-	object.setFloatAttribute('cat_wpn_damage.wpn_attack_speed', 0.4)
-	object.setStringAttribute('cat_wpn_damage.wpn_damage_type', 'Energy')
-	object.setStringAttribute('cat_wpn_damage.damage', '250-500')
-	object.setStringAttribute('cat_wpn_damage.wpn_range', '0-35m')
-	object.setStringAttribute('cat_wpn_damage.wpn_category', 'Pistol')
-	object.setIntAttribute('cat_wpn_damage.dps', object.getDamagePerSecond())
+	object.setIntAttribute('required_combat_level', 88)	
+	object.setAttackSpeed(0.4);
+	object.setMaxRange(35);
+	object.setDamageType("energy");
+	object.setMinDamage(250);
+	object.setMaxDamage(500);
+	object.setWeaponType(2);
 	return
