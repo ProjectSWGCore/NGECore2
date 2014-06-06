b22c67e4e1a01eb3b0d9224facb3bd7cd69af86c
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-06-06 17:40:38 -0400

    Fixed Some Armor Bugs

--- scripts/object/tangible/wearables/armor/tantel/armor_medic_roadmap_gloves_02_01.py
+++ scripts/object/tangible/wearables/armor/tantel/armor_medic_roadmap_gloves_02_01.py
@@ -8,10 +8,4 @@ def setup(core, object):
 	object.setIntAttribute('cat_stat_mod_bonus.@stat_n:constitution_modified', 3)
 	object.setStringAttribute('class_required', 'Medic')
 	object.setStringAttribute('armor_category', '@obj_attr_n:armor_reconnaissance')
-	object.setIntAttribute('cat_armor_standard_protection.kinetic', 640)
-	object.setIntAttribute('cat_armor_standard_protection.energy', 2640)
-	object.setIntAttribute('cat_armor_special_protection.special_protection_type_heat', 1640)
-	object.setIntAttribute('cat_armor_special_protection.special_protection_type_cold', 1640)
-	object.setIntAttribute('cat_armor_special_protection.special_protection_type_acid', 1640)
-	object.setIntAttribute('cat_armor_special_protection.special_protection_type_electricity', 1640)
 	return	
