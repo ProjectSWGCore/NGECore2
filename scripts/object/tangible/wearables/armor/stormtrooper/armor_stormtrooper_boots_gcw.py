54607be2e2a6fd147919d0eb459e22a566ae0078
CharonInferar <CharonInferar@aol.de>
2014-06-08 01:28:38 +0200

    fix
    fix

--- scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_boots_gcw.py
+++ scripts/object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_boots_gcw.py
@@ -2,9 +2,5 @@ import sys
 
 def setup(core, object):
 	object.setStringAttribute('required_faction', 'Imperial')
-<<<<<<< HEAD
-	object.setStringAttribute('armor_category', 'Battle')
-=======
 	object.setStringAttribute('armor_category', '@obj_attr_n:armor_battle')
->>>>>>> origin/master
 	return	
--- src/services/pet/MountService.java
+++ src/services/pet/MountService.java
@@ -676,10 +676,7 @@ public class MountService implements INetworkDispatch {
 		if (isMounted(owner, mount)) {
 			//storer.sendSystemMessage(OutOfBand.ProsePackage("@pet/pet_menu:must_dismount"), DisplayType.Broadcast);
 			//return;
-<<<<<<< HEAD
-=======
 			dismount(storer, mount);
->>>>>>> origin/master
 		}
 		
 		if (owner.getTefTime() > 0) {
