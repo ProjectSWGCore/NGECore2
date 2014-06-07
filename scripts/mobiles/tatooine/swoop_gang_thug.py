6e608a25c5b60a94c3da897213db03de3be05064
Ziggeh <ziggy@projectswg.com>
2014-06-07 02:25:41 +0200

    MobileTemplate weapon refactor

--- scripts/mobiles/tatooine/swoop_gang_thug.py
+++ scripts/mobiles/tatooine/swoop_gang_thug.py
@@ -1,6 +1,9 @@
 import sys
 from services.spawn import MobileTemplate
 from services.spawn import WeaponTemplate
+from resources.datatables import WeaponType
+from resources.datatables import Difficulty
+from resources.datatables import Options
 from java.util import Vector
 
 def addTemplate(core):
@@ -8,10 +11,7 @@ def addTemplate(core):
 	
 	mobileTemplate.setCreatureName('tatooine_opening_smuggler_09')
 	mobileTemplate.setLevel(8)
-	mobileTemplate.setDifficulty(0)
-	mobileTemplate.setAttackRange(12)
-	mobileTemplate.setAttackSpeed(1.0)
-	mobileTemplate.setWeaponType(0)
+	mobileTemplate.setDifficulty(Difficulty.NORMAL)
 	mobileTemplate.setMinSpawnDistance(4)
 	mobileTemplate.setMaxSpawnDistance(8)
 	mobileTemplate.setDeathblow(False)
@@ -19,7 +19,7 @@ def addTemplate(core):
 	mobileTemplate.setSocialGroup("niko thug")
 	mobileTemplate.setAssistRange(4)
 	mobileTemplate.setStalker(False)
-	mobileTemplate.setOptionsBitmask(192)
+	mobileTemplate.setOptionsBitmask(Options.AGGRESSIVE + Options.ATTACKABLE)
 		
 	templates = Vector()
 	templates.add('object/mobile/shared_dressed_criminal_swooper_human_female_01.iff')
@@ -28,8 +28,8 @@ def addTemplate(core):
 	templates.add('object/mobile/shared_dressed_criminal_swooper_zabrak_male_01.iff')
 	mobileTemplate.setTemplates(templates)
 	
-	weaponTemplates = Vector()
-	weapontemplate = WeaponTemplate('object/weapon/ranged/pistol/shared_pistol_cdef.iff', 2, 1.0)
+	weaponTemplates = Vector()	
+	weaponTemplate = WeaponTemplate('object/weapon/ranged/pistol/shared_pistol_cdef.iff', WeaponType.PISTOL, 1.0, 12, 'energy')
 	weaponTemplates.add(weapontemplate)
 	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
 	
@@ -38,4 +38,5 @@ def addTemplate(core):
 	mobileTemplate.setAttacks(attacks)
 	
 	core.spawnService.addMobileTemplate('swoop_gang_thug', mobileTemplate)
+
 	return
