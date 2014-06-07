1c1e9f0d6d540f611ca53054f39d24e45dab7238
tacef <neuwirth.christoph@gmx.net>
2014-06-07 21:39:52 +0200

    Fixed #692

--- scripts/object/building/military/small_imperial_cloning_facility_s02.py
+++ scripts/object/building/military/small_imperial_cloning_facility_s02.py
@@ -1,4 +1,16 @@
 import sys
+from java.util import Vector
+from resources.common import SpawnPoint
 
 def setup(core, object):
+	objSvc = core.objectService
+	objSvc.createChildObject(object, 'object/tangible/terminal/shared_terminal_cloning.iff', float(-5.5), float(0.13), float(-3.44), float(0.70), float(0.71), 6)
+
+	spawnPoints = Vector()
+	spawnPoints.add(SpawnPoint(4.45, 0.13, -3.80, -0.71, 0.71, 5))
+	spawnPoints.add(SpawnPoint(4.65, 0.13, 1.50, 0.71, -0.70, 4))
+	spawnPoints.add(SpawnPoint(-3.99, 0.13, -3.52, 0.70, 0.71, 6))
+
+	object.setAttachment('spawnPoints', spawnPoints)
+
 	return
