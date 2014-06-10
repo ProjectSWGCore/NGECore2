fdcf8a1058155efb84867d772d984d5a1d75f238
CharonInferar <CharonInferar@aol.de>
2014-06-10 15:14:22 +0200

    fix

--- scripts/commands/resourcecontainersplit.py
+++ scripts/commands/resourcecontainersplit.py
@@ -4,6 +4,5 @@ def setup():
     return
     
 def run(core, actor, target, commandString):
-	owner.sendSystemMessage('option ', 0)
     target.splitContainer(actor,target,commandString)
     return
