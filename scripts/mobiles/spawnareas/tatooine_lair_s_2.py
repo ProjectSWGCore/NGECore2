eb83dde9307d44de9003c32a13a413e1fd87a545
Bronco69 <petertigges1977@googlemail.com>
2014-06-13 01:20:33 +0200

    Tatooine planetary spawns
    Put in only starter npcs for now

---
+++ scripts/mobiles/spawnareas/tatooine_lair_s_2.py
@@ -0,0 +1,6 @@
+# Spawn Area file created with PSWG Planetary Spawn Tool
+import sys
+
+def addSpawnArea(core):
+	core.spawnService.addLairSpawnArea('tatooine_starter', 3700, -7000, 1500, 'tatooine')
+	return
---
+++ scripts/mobiles/spawnareas/tatooine_lair_se_1.py
@@ -0,0 +1,6 @@
+# Spawn Area file created with PSWG Planetary Spawn Tool
+import sys
+
+def addSpawnArea(core):
+	core.spawnService.addLairSpawnArea('tatooine_starter', 6300, -6400, 2200, 'tatooine')
+	return
