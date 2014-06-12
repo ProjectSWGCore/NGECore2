577c10eeceb3f89497c1e3c90dd286721fd336ff
Bronco69 <petertigges1977@googlemail.com>
2014-06-13 01:19:52 +0200

    Naboo planetary spawns
    Put in only starter npcs for now

---
+++ scripts/mobiles/spawnareas/naboo_lair_center_3.py
@@ -0,0 +1,6 @@
+# Spawn Area file created with PSWG Planetary Spawn Tool
+import sys
+
+def addSpawnArea(core):
+	core.spawnService.addLairSpawnArea('naboo_starter', -200, 2200, 800, 'naboo')
+	return
---
+++ scripts/mobiles/spawnareas/naboo_lair_e_1.py
@@ -0,0 +1,6 @@
+# Spawn Area file created with PSWG Planetary Spawn Tool
+import sys
+
+def addSpawnArea(core):
+	core.spawnService.addLairSpawnArea('naboo_starter', 4900, -1200, 3700, 'naboo')
+	return
