ec1d89bbc70d929fbeb33ddbf7821a0eb87d5e90
Zing <jeremy@zingstecharchive.com>
2014-06-12 23:51:44 -0700

    Junk Loot A-D
    Correct templates and names

---
+++ scripts/loot/lootItems/junk/burnt_out_droid_motivator.py
@@ -0,0 +1,20 @@
+def itemTemplate():
+
+	return ['object/tangible/loot/npc_loot/shared_motivator_device_generic.iff']
+
+def customItemName():
+
+	return 'Burnt Out Droid Motivator'
+
+def stackable():
+
+	return 1
+	
+def junkDealerPrice():
+
+	return 28
+	
+def junkType():
+
+	return 0
+	
---
+++ scripts/loot/lootItems/junk/canceled_travel_ticket.py
@@ -0,0 +1,21 @@
+
+def itemTemplate():
+
+	return ['object/tangible/loot/misc/shared_ticket_expired.iff']
+
+def customItemName():
+
+	return 'Canceled Travel Ticket'
+
+def stackable():
+
+	return 1
+	
+def junkDealerPrice():
+
+	return 28
+	
+def junkType():
+
+	return 0
+	
