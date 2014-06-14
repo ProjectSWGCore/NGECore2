0d89c4db2c64bbc23043f0bc1cd6ddd06f7f253d
tacef <neuwirth.christoph@gmx.net>
2014-06-14 20:54:16 +0200

    Added Content to Corellia Planetary Map

--- scripts/object/building/corellia/shuttleport_corellia.py
+++ scripts/object/building/corellia/shuttleport_corellia.py
@@ -6,5 +6,8 @@ def setup(core, object):
 	objSvc.createChildObject(object, 'object/tangible/terminal/shared_terminal_travel.iff', -13, 0.6, -10, -0.707107, 0.707107)
 	objSvc.createChildObject(object, 'object/creature/npc/theme_park/shared_player_shuttle.iff', 0, 0.6, 0, 1, 0)
 	objSvc.createChildObject(object, 'object/tangible/travel/ticket_collector/shared_ticket_collector.iff', -13, 0.6, 10, -0.707107, 0.707107)	
-
+	
+	
+	core.mapService.addLocation(object.getPlanet(), 'Shuttleport', object.getPosition().x, object.getPosition().z, 14, 0, 0)
+	
 	return
