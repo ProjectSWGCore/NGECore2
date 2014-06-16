348190ab91151f34501a0799fdaa09cb95833965
tacef <neuwirth.christoph@gmx.net>
2014-06-16 19:30:04 +0200

    Populated Slumthief spot outside Mos Espa

---
+++ scripts/static_spawns/tatooine/mos_espa_slum_thiefs.py
@@ -0,0 +1,40 @@
+import sys
+# Project SWG:   Anchorhead:  Static Spawns
+# (C)2014 ProjectSWG
+
+
+
+from resources.datatables import Options
+from resources.datatables import State
+
+def addPlanetSpawns(core, planet):
+
+
+	stcSvc = core.staticService
+	objSvc = core.objectService
+	
+	# TODO Check all NPCs for personalized scripting, change format.
+
+	
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3165), float(5), float(2154), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3162), float(5), float(2174), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3175), float(5), float(2165), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3176), float(5), float(2149), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3185), float(5), float(2141), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3153), float(5), float(2149), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3161), float(5), float(2145), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3173), float(5), float(2139), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3178), float(5), float(2135), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3190), float(5), float(2138), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3193), float(5), float(2146), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3185), float(5), float(2155), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3187), float(5), float(2163), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3163), float(5), float(2162), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3176), float(5), float(2186), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3172), float(5), float(2189), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3189), float(5), float(2177), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3190), float(5), float(2175), float(0), float(0), float(0), float(0), 45)
+	stcSvc.spawnObject('slum_thief', 'tatooine', long(0), float(-3194), float(5), float(2169), float(0), float(0), float(0), float(0), 45)
+	
+	return
+	
