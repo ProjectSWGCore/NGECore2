f7a37de956e108396309ed28575f7d31da55b0ed
Mesagoppinmypants <pressinger.gene66@gmail.com>
2014-06-19 09:42:49 -0400

    Fixed Set Bonus Bug

--- scripts/object/tangible/wearables/ring/item_band_set_jedi_utility_b_01_01.py
+++ scripts/object/tangible/wearables/ring/item_band_set_jedi_utility_b_01_01.py
@@ -11,8 +11,8 @@ def setup(core, object):
 	object.setIntAttribute('required_combat_level', 85)
 	object.setIntAttribute('cat_skill_mod_bonus.@stat_n:expertise_glancing_blow_all', 1)
 	object.setIntAttribute('cat_skill_mod_bonus.@stat_n:expertise_critical_hit_reduction', 1)
-	object.setStringAttribute('@set_bonus:piece_bonus_count_3', '@set_bonus:set_jedi_utility_b_1')
-	object.setStringAttribute('@set_bonus:piece_bonus_count_4', '@set_bonus:set_jedi_utility_b_2')
-	object.setStringAttribute('@set_bonus:piece_bonus_count_5', '@set_bonus:set_jedi_utility_b_3')
-	object.setAttachment('setBonus', 'set_jedi_utility_b')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_3', '@set_bonus:set_bonus_jedi_utility_b_1')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_4', '@set_bonus:set_bonus_jedi_utility_b_2')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_5', '@set_bonus:set_bonus_jedi_utility_b_3')
+	object.setAttachment('setBonus', 'set_bonus_jedi_utility_b')
 	return
