213a068c286debc2492e0d35142cf30f8229d62d
Zing <jeremy@zingstecharchive.com>
2014-06-21 00:49:31 -0700

    Loot collection will auto delete if successfully added.

---
+++ scripts/loot/lootItems/collections/tusken_valuables/scavenged_metal.py
@@ -0,0 +1,20 @@
+
+def itemTemplate():
+
+	return ['object/tangible/loot/creature_loot/collections/shared_tusken_metal.iff']
+	
+def STFparams():
+
+	return ['static_item_n','col_tusken_metal_02_01','static_item_d','col_tusken_metal_02_01']
+
+def AddToCollection():
+
+	return 'col_tusken_valuables'
+	
+def CollectionItemName():
+
+	return 'tusken_metal_01'
+
+def stackable():
+
+	return 1
---
+++ scripts/loot/lootItems/collections/tusken_valuables/tusken_talisman.py
@@ -0,0 +1,20 @@
+
+def itemTemplate():
+
+	return ['object/tangible/loot/creature_loot/collections/shared_tusken_talisman.iff']
+	
+def STFparams():
+
+	return ['static_item_n','col_tusken_talisman_02_01','static_item_d','col_tusken_talisman_02_01']
+
+def AddToCollection():
+
+	return 'col_tusken_valuables'
+	
+def CollectionItemName():
+
+	return 'tusken_talisman_01'
+
+def stackable():
+
+	return 1
