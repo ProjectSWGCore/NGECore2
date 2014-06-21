8d1cc1722061e28b68927a9faa8d86c83ac2f2a4
Zing <jeremy@zingstecharchive.com>
2014-06-21 15:18:17 -0700

    More Loot Collections
    Moved loot collection radial into collection radial (can be used for
    other consume item collections.
    added: Nightsister Valuables, Gem, Prototype IG-88, and StormTrooper
    collections.

---
+++ scripts/loot/lootItems/collections/prototype_stormtrooper/stormtrooper_stand.py
@@ -0,0 +1,20 @@
+
+def itemTemplate():
+
+	return ['object/tangible/loot/creature_loot/collections/shared_trader_mun_stand_base.iff']
+	
+def STFparams():
+
+	return ['static_item_n','col_stormtrooper_metal_stand_02_01','static_item_d','col_stormtrooper_metal_stand_02_01']
+
+def AddToCollection():
+
+	return 'col_mun_trader'
+	
+def CollectionItemName():
+
+	return 'trader_mun_stand_base'
+
+def stackable():
+
+	return 1
---
+++ scripts/loot/lootItems/collections/prototype_stormtrooper/stormtrooper_wooden_dowel.py
@@ -0,0 +1,12 @@
+
+def itemTemplate():
+
+	return ['object/tangible/loot/creature_loot/collections/shared_stormtrooper_wooden_dowel.iff']
+	
+def STFparams():
+
+	return ['static_item_n','col_stormtrooper_wooden_dowel_02_01','static_item_d','col_stormtrooper_wooden_dowel_02_01']
+
+def stackable():
+
+	return 1
