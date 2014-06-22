b6fd2643c8ce991c399f9a66bb7c4027900a09fb
Zing <jeremy@zingstecharchive.com>
2014-06-21 21:15:42 -0700

    Loot Collections
    DIY: Glass Shelves
    Holographic Emitter
    Cabinetry Tech
    Craftsman Tools
    Tech in Maximizing Space
    Shelving Tech
    Storage Tech

---
+++ scripts/loot/lootItems/collections/housing_improvements/craftsman_tools/craftsman_tools_8.py
@@ -0,0 +1,29 @@
+def itemTemplate():
+
+	return ['object/tangible/loot/creature_loot/collections/shared_housing_improvement_03.iff']
+
+def lootDescriptor():
+
+    return 'customattributes'
+        
+def customizationAttributes():
+    return ['/private/index_color_1']
+    
+def customizationValues():
+    return [8]  
+    
+def STFparams():
+
+	return ['static_item_n','item_collection_housing_improvement_03_08','static_item_d','item_collection_housing_improvement_03_08']
+
+def AddToCollection():
+
+	return 'housing_improvement_03'
+		
+def CollectionItemName():
+
+	return 'housing_improvement_03_08'
+
+def stackable():
+
+	return 1
