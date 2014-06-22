74612814a71093e822bf23623105712568fa4329
Zing <jeremy@zingstecharchive.com>
2014-06-22 11:23:39 -0700

    example of world collection setup

--- scripts/object/tangible/collection/rare_pistol_blackfalcon.py
+++ scripts/object/tangible/collection/rare_pistol_blackfalcon.py
@@ -1,4 +1,7 @@
 import sys
 
 def setup(core, object):
+	print ('test')
+	object.setAttachment('CollectionItemName', 'blackfalcon_01')
+	object.setAttachment('radial_filename', 'item/world_collection')
 	return
