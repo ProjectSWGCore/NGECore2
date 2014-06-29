9d0bf56efe700e1b8e4d470607cd79543664dcb2
Zing <jeremy@zingstecharchive.com>
2014-06-28 20:23:39 -0700

    Rebel rank reward system

--- scripts/object/tangible/loot/generic_usable/binoculars_s1_generic.py
+++ scripts/object/tangible/loot/generic_usable/binoculars_s1_generic.py
@@ -1,4 +1,10 @@
 import sys
 
 def setup(core, object):
+	object.setAttachment('radial_filename', 'object/usable')
+	return
+	
+def use(core, actor, object):
+	if object.getStfName() == ('item_consumable_detect_hidden_02_04'):
+		core.buffService.addBuffToCreature(actor, 'detectHiddenConsumable100', actor)
 	return
--- scripts/object/weapon/ranged/pistol/pistol_pvp.py
+++ scripts/object/weapon/ranged/pistol/pistol_pvp.py
@@ -1,4 +1,22 @@
 import sys
 
 def setup(core, object):
+	object.setStfFilename('static_item_n')
+	object.setStfName('weapon_pistol_pvp_general_reward_06_01')
+	object.setDetailFilename('static_item_d')
+	object.setDetailName('weapon_pistol_pvp_general_reward_06_01')
+	object.setStringAttribute('tier', '6')
+	object.setIntAttribute('no_trade', 1)
+	object.setIntAttribute('cat_stat_mod_bonus.@stat_n:constitution_modified', 10)
+	object.setIntAttribute('cat_stat_mod_bonus.@stat_n:precision_modified', 10)
+	object.setStringAttribute('healing_combat_level_required', '75')
+	object.setStringAttribute('required_skill', 'None')
+	object.setDamageType("energy");
+	object.setWeaponType(2);
+	object.setAttackSpeed(0.4);
+	object.setMinDamage(259);
+	object.setMaxDamage(518);
+	object.setStringAttribute('wpn_elemental_type', 'Electricity')
+	object.setStringAttribute('wpn_elemental_value', '20')
+	object.setMaxRange(35);	
 	return
