1e5e997755f76cd9331a6b7a073d0e913702ad25
Ziggeh <ziggy@projectswg.com>
2014-06-30 01:47:06 +0200

    Added Advanced Electrobinoculars

---
+++ scripts/object/tangible/loot/generic_usable/item_consumable_detect_hidden_02_02.py
@@ -0,0 +1,9 @@
+import sys
+
+def setup(core, object):
+	object.setAttachment('radial_filename', 'object/usable')
+	object.setStfFilename('static_item_n')
+	object.setStfName('item_consumable_detect_hidden_02_02')
+	object.setDetailFilename('static_item_d')
+	object.setDetailName('item_consumable_detect_hidden_02_02')
+	return
