cda6332745b71c709e648986b5aa054036cc8ca3
Ziggeh <ziggy@projectswg.com>
2014-06-30 17:23:29 +0200

    Added Lava Crystal item

--- scripts/object/tangible/component/weapon/lightsaber/item_color_crystal_02_20.py
+++ scripts/object/tangible/component/weapon/lightsaber/item_color_crystal_02_20.py
@@ -11,5 +11,6 @@ def setup(core, object):
 	object.setStringAttribute('condition', '1000/1000')
 	object.setStringAttribute("@obj_attr_n:color", "Windu's Guile")
 	object.setAttachment('radial_filename', 'item/tunable')
+	object.setStringAttribute('@obj_attr_n:crystal_owner", "\\#D1F56F UNTUNED \\#FFFFFF ')
 	return
 	
--- scripts/object/tangible/component/weapon/lightsaber/lightsaber_module_lava_crystal.py
+++ scripts/object/tangible/component/weapon/lightsaber/lightsaber_module_lava_crystal.py
@@ -1,4 +1,8 @@
 import sys
 
 def setup(core, object):
+	object.setStringAttribute('condition', '100/100')
+	object.setStringAttribute('@obj_attr_n:color', 'Lava')
+	object.setAttachment('radial_filename', 'item/tunable')
 	return
+	
