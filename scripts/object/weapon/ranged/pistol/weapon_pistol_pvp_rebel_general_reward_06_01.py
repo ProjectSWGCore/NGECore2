baa61a543f96d5720d07284bbaa5436f9e76c01b
Ziggeh <ziggy@projectswg.com>
2014-06-30 14:25:11 +0200

    Fixed several GCW reward weapons

--- scripts/object/weapon/ranged/pistol/weapon_pistol_pvp_rebel_general_reward_06_01.py
+++ scripts/object/weapon/ranged/pistol/weapon_pistol_pvp_rebel_general_reward_06_01.py
@@ -7,10 +7,10 @@ def setup(core, object):
 	object.setStfName('weapon_pistol_pvp_rebel_general_reward_06_01')
 	object.setDetailFilename('static_item_d')
 	object.setDetailName('weapon_pistol_pvp_rebel_general_reward_06_01')
-	object.setAttackSpeed(0.4);
-	object.setMaxRange(35);
-	object.setDamageType('energy');
-	object.setMinDamage(275);
-	object.setMaxDamage(518);
-	object.setWeaponType(WeaponType.PISTOL);
+	object.setAttackSpeed(0.4)
+	object.setMaxRange(35)
+	object.setDamageType('energy')
+	object.setMinDamage(275)
+	object.setMaxDamage(518)
+	object.setWeaponType(WeaponType.PISTOL)
 	return
