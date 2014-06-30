be9650db6b9b082fd2df8ac400ba9d69e43d6bf0
Zing <jeremy@zingstecharchive.com>
2014-06-29 20:47:13 -0700

    Fixed a few rebel rank reward items
    Rebel recruiter rewards can now be called by conversing with the
    recruiter again.
    Exceptional pistol is now deposited in inventory
    Binocular buff now adds to player
    Rebel Command Barc now generates correct vehicle

---
+++ scripts/object/tangible/deed/vehicle_deed/item_deed_barc_rebel_06_01.py
@@ -0,0 +1,15 @@
+import sys
+
+def setup(core, object):
+	object.setAttachment('radial_filename', 'deeds/vehicleDeed')
+	object.setStfFilename('static_item_n')
+	object.setStfName('item_deed_barc_rebel_06_01')
+	object.setDetailFilename('static_item_d')
+	object.setDetailName('item_deed_barc_rebel_06_01')
+	object.setIntAttribute('no_trade', 1)
+	return
+
+def use(core, actor, object):
+	core.mountService.generateVehicle(actor, object, 'object/mobile/vehicle/shared_barc_speeder_rebel.iff', 'object/intangible/vehicle/shared_barc_speeder_rebel_pcd.iff')
+	return
+	
