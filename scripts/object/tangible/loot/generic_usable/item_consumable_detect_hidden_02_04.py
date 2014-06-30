56edcdc95c4ad47a5c072b9be965b1918c73e976
Ziggeh <ziggy@projectswg.com>
2014-06-30 16:25:08 +0200

    Fixed factional electrobinoculars

--- scripts/object/tangible/loot/generic_usable/item_consumable_detect_hidden_02_04.py
+++ scripts/object/tangible/loot/generic_usable/item_consumable_detect_hidden_02_04.py
@@ -7,12 +7,8 @@ def setup(core, object):
 	object.setDetailFilename('static_item_d')
 	object.setDetailName('item_consumable_detect_hidden_02_04')
 	object.setIntAttribute('reuse_time', 5)
-	object.setIntAttribute('num_in_stack', 8)
+	object.setUses(8)
 	object.setStringAttribute('proc_name', '@ui_buff:detecthiddenconsumable100')
+	object.setAttachment('alternateBuffName', 'detectHiddenConsumable100')
 	return
-	
-def use(core, actor, object):
-	core.buffService.addBuffToCreature(actor, 'detectHiddenConsumable100', actor)
-	return
-	
 	
