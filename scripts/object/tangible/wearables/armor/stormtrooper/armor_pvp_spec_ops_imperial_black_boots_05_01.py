79944e09a950cb1df6184b491cccb2129385e521
Zing <jeremy@zingstecharchive.com>
2014-06-30 23:09:44 -0700

    Fixed Tirtiary color on Imperial Spec Ops Armor. Added Tirtiary option to recolorable radial.

--- scripts/object/tangible/wearables/armor/stormtrooper/armor_pvp_spec_ops_imperial_black_boots_05_01.py
+++ scripts/object/tangible/wearables/armor/stormtrooper/armor_pvp_spec_ops_imperial_black_boots_05_01.py
@@ -3,6 +3,7 @@ import sys
 def setup(core, object):
 	object.setCustomizationVariable('/private/index_color_1', 15);
 	object.setCustomizationVariable('/private/index_color_2', 15);
+	object.setCustomizationVariable('/private/index_color_3', 2);
 	object.setStfFilename('static_item_n')
 	object.setStfName('armor_pvp_spec_ops_imperial_black_boots_05_01')
 	object.setDetailFilename('static_item_d')
