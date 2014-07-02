9d1d7f096579160cf2e17a7ede236008f92ab8cd
Bronco69 <petertigges1977@googlemail.com>
2014-07-02 03:27:09 +0200

    Added correct radial for all vehicles

--- scripts/object/intangible/vehicle/barc_speeder_pcd.py
+++ scripts/object/intangible/vehicle/barc_speeder_pcd.py
@@ -1,15 +1,5 @@
 import sys
 
 def setup(core, object):
-	object.setAttachment('radial_filename', 'vehicle_pcd')
-	return
-
-def use(core, actor, object):
-	if object and actor.getAttachment('activeVehicleID') == None:
-		barc = core.objectService.createObject('object/mobile/vehicle/shared_barc_speeder.iff', 0, actor.getPlanet(), actor.getPosition(), actor.getOrientation())
-		
-		barc.setOwnerId(actor.getObjectID())
-		actor.setAttachment('activeVehicleID', barc.getObjectID())
-		
-		core.simulationService.add(barc, barc.getPosition().x, barc.getPosition().z, True)
+	object.setAttachment('radial_filename', 'datapad/vehicle_pcd')
 	return
