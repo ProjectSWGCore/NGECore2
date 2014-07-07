0400679ce0b8dddaf162177a85f1b38ae6c0ecc7
Freddiec <freddylikespotatos@gmail.com>
2014-07-06 21:03:08 -0500

    [Added] Vehicles, TCG and normal ones

--- scripts/object/tangible/deed/vehicle_deed/landspeeder_v35_deed.py
+++ scripts/object/tangible/deed/vehicle_deed/landspeeder_v35_deed.py
@@ -1,4 +1,9 @@
 import sys
 
 def setup(core, object):
+	object.setAttachment('radial_filename', 'deeds/vehicleDeed')
+	return
+
+def use(core, actor, object):
+	core.mountService.generateVehicle(actor, object, 'object/mobile/vehicle/shared_landspeeder_v35.iff', 'object/intangible/vehicle/shared_landspeeder_v35_pcd.iff')
 	return
