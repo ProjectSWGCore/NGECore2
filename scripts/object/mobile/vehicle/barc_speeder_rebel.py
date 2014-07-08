aa1169977babe7f1f85b3ecbf98d0cfa6b205bc5
Bronco69 <petertigges1977@googlemail.com>
2014-07-08 22:18:42 +0200

    Fixed mobile templates for vehicles

--- scripts/object/mobile/vehicle/barc_speeder_rebel.py
+++ scripts/object/mobile/vehicle/barc_speeder_rebel.py
@@ -1,16 +1,10 @@
 import sys
-
+	
 def setup(core, object):
-	object.setAttachment('radial_filename', 'creature/vehicle')
+	core.buffService.addBuffToCreature(object, 'vehicle_pvp_barc_rebel', object)
+	object.setAttachment('radial_filename', 'creature/vehicle')	
 	return
 
 def use(core, actor, object):
-	vehicle = actor.getContainer()
-	
-	if vehicle:
-		if vehicle.getObjectID() == object.getObjectID():
-			vehicle.remove(actor)
-		else:
-			vehicle.add(actor)
-	
+	core.buffService.addBuffToCreature(object, 'vehicle_pvp_barc_rebel', object)
 	return
--- scripts/object/mobile/vehicle/landspeeder_tantive4.py
+++ scripts/object/mobile/vehicle/landspeeder_tantive4.py
@@ -1,20 +1,5 @@
 import sys
 
 def setup(core, object):
-	object.setAttachment('radial_filename', 'vehicle')
-	return
-
-def use(core, actor, object):
-	vehicle = actor.getContainer()
-	
-	if vehicle:
-		if vehicle.getObjectID() == object.getObjectID():
-			vehicle.remove(actor)
-		else:
-			vehicle.add(actor)
-	
-	return
-import sys
-
-def setup(core, object):
+	object.setAttachment('radial_filename', 'creature/vehicle')
 	return
