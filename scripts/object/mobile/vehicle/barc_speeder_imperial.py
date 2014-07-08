b671a2c00c52367a32664cf6a37498651265c9a2
Bronco69 <petertigges1977@googlemail.com>
2014-07-08 22:18:42 +0200

    Fixed mobile templates for vehicles

--- scripts/object/mobile/vehicle/barc_speeder_imperial.py
+++ scripts/object/mobile/vehicle/barc_speeder_imperial.py
@@ -1,7 +1,8 @@
 import sys
-
+	
 def setup(core, object):
 	core.buffService.addBuffToCreature(object, 'vehicle_pvp_barc_imperial', object)
+	object.setAttachment('radial_filename', 'creature/vehicle')	
 	return
 
 def use(core, actor, object):
