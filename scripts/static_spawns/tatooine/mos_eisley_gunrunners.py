bec4a3a25cdddc82583e75caaa5373798ce70c42
tacef <neuwirth.christoph@gmx.net>
2014-07-15 21:50:05 +0200

    Changed Some NPC spawn points to use loiter

--- scripts/static_spawns/tatooine/mos_eisley_gunrunners.py
+++ scripts/static_spawns/tatooine/mos_eisley_gunrunners.py
@@ -1,17 +1,17 @@
-import sys
 # Project SWG:  Mos Eisley gunrunners:  Static Spawns
 # (C)2014 ProjectSWG
 
-
-
+import sys
 from resources.datatables import Options
 from resources.datatables import State
+from java.util import Vector
+from engine.resources.scene import Point3D
 
 def addPlanetSpawns(core, planet):
 
-
 	stcSvc = core.staticService
 	objSvc = core.objectService
+	aiSvc = core.aiService
 
 	# gunrunners
 	gunrunner = stcSvc.spawnObject('gunrunner', 'tatooine', long(0), float(2835), float(8), float(-4276), float(0), float(0), float(0), float(0), 45)
@@ -34,5 +34,23 @@ def addPlanetSpawns(core, planet):
 	gunrunner_sentry3 = stcSvc.spawnObject('gunrunner_sentry', 'tatooine', long(0), float(2833), float(10), float(-4252), float(0), float(0), float(0), float(0), 45)
 	gunrunner_sentry4 = stcSvc.spawnObject('gunrunner_sentry', 'tatooine', long(0), float(2830), float(6), float(-4308), float(0), float(0), float(0), float(0), 45)
 	
+	aiSvc.setLoiter(gunrunner, float(1), float(8))
+	aiSvc.setLoiter(gunrunner1, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner2, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner3, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner4, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner5, float(1), float(8))  
+	aiSvc.setLoiter(gunrunner6, float(1), float(8))
+	aiSvc.setLoiter(gunrunner7, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner8, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner9, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner10, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner11, float(1), float(8))
+	
+	aiSvc.setLoiter(gunrunner_sentry, float(1), float(8))
+	aiSvc.setLoiter(gunrunner_sentry1, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner_sentry2, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner_sentry3, float(1), float(8)) 
+	aiSvc.setLoiter(gunrunner_sentry4, float(1), float(8)) 
 	
 	return	
--- scripts/static_spawns/tatooine/mos_eisley_mites.py
+++ scripts/static_spawns/tatooine/mos_eisley_mites.py
@@ -1,18 +1,19 @@
-import sys
+
 # Project SWG:   MosEisley mites:  Static Spawns
 # (C)2014 ProjectSWG
 
-
-
+import sys
 from resources.datatables import Options
 from resources.datatables import State
+from java.util import Vector
+from engine.resources.scene import Point3D
 
 def addPlanetSpawns(core, planet):
 
 
 	stcSvc = core.staticService
 	objSvc = core.objectService
-	
+	aiSvc = core.aiService
 	
 	# Mound Mites
 	mound_mite1 = stcSvc.spawnObject('mound_mite', 'tatooine', long(0), float(3785), float(57), float(-5057), float(0), float(0), float(0), float(0), 30)	
@@ -32,7 +33,6 @@ def addPlanetSpawns(core, planet):
 	mound_mite25 = stcSvc.spawnObject('mound_mite', 'tatooine', long(0), float(3555), float(63), float(-4345), float(0), float(0), float(0), float(0), 30)	
 	mound_mite26 = stcSvc.spawnObject('mound_mite', 'tatooine', long(0), float(3565), float(63), float(-4325), float(0), float(0), float(0), float(0), 30)	
 	
-	
 	# Legacy RockMites
 	
 	rockmite = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3140), float(5), float(-4907), float(0), float(0), float(0), float(0), 30)	
@@ -48,7 +48,6 @@ def addPlanetSpawns(core, planet):
 	rockmite21 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3724), float(5), float(-4661), float(0), float(0), float(0), float(0), 30)	
 	rockmite22 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3733), float(5), float(-4659), float(0), float(0), float(0), float(0), 30)	
 	rockmite23 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3747), float(5), float(-4655), float(0), float(0), float(0), float(0), 30)	
-	rockmite24 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3752), float(5), float(-4645), float(0), float(0), float(0), float(0), 30)	
 	rockmite25 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3761), float(5), float(-4644), float(0), float(0), float(0), float(0), 30)	
 	rockmite26 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3762), float(5), float(-4654), float(0), float(0), float(0), float(0), 30)	
 	rockmite27 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3732), float(5), float(-4664), float(0), float(0), float(0), float(0), 30)	
@@ -57,6 +56,40 @@ def addPlanetSpawns(core, planet):
 	rockmite210 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3769), float(5), float(-4650), float(0), float(0), float(0), float(0), 30)	
 	rockmite211 = stcSvc.spawnObject('rockmite', 'tatooine', long(0), float(3778), float(5), float(-4647), float(0), float(0), float(0), float(0), 30)	
 
+	aiSvc.setLoiter(mound_mite1, float(1), float(8))
+	aiSvc.setLoiter(mound_mite11, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite12, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite13, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite14, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite15, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite16, float(1), float(8)) 
+	
+	aiSvc.setLoiter(mound_mite2, float(1), float(8))  
+	aiSvc.setLoiter(mound_mite21, float(1), float(8))
+	aiSvc.setLoiter(mound_mite22, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite23, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite24, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite25, float(1), float(8)) 
+	aiSvc.setLoiter(mound_mite26, float(1), float(8)) 
+	
+	aiSvc.setLoiter(rockmite, float(1), float(8)) 
+	aiSvc.setLoiter(rockmite1, float(1), float(8))
+	aiSvc.setLoiter(rockmite2, float(1), float(8)) 
+	aiSvc.setLoiter(rockmite3, float(1), float(8))
+	aiSvc.setLoiter(rockmite4, float(1), float(8))
+	aiSvc.setLoiter(rockmite5, float(1), float(8))
+	aiSvc.setLoiter(rockmite6, float(1), float(8))
 
+	aiSvc.setLoiter(rockmite2, float(1), float(8)) 
+	aiSvc.setLoiter(rockmite21, float(1), float(8))
+	aiSvc.setLoiter(rockmite22, float(1), float(8)) 
+	aiSvc.setLoiter(rockmite23, float(1), float(8))
+	aiSvc.setLoiter(rockmite25, float(1), float(8))
+	aiSvc.setLoiter(rockmite26, float(1), float(8))
+	aiSvc.setLoiter(rockmite27, float(1), float(8))
+	aiSvc.setLoiter(rockmite28, float(1), float(8)) 
+	aiSvc.setLoiter(rockmite29, float(1), float(8))
+	aiSvc.setLoiter(rockmite210, float(1), float(8))
+	aiSvc.setLoiter(rockmite211, float(1), float(8))
 	
 	return
