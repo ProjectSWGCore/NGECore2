71f4bcf945fe9b5ee09307efdacdf3b8f5b3fcfc
tacef <neuwirth.christoph@gmx.net>
2014-07-20 20:12:33 +0200

    Changed Bestine spawns to use mobiletemplates
    Added several NPC scripts for bestine

---
+++ scripts/mobiles/generic/static/tatooine/talmont.py
@@ -0,0 +1,36 @@
+import sys
+from services.spawn import MobileTemplate
+from services.spawn import WeaponTemplate
+from resources.datatables import WeaponType
+from resources.datatables import Difficulty
+from resources.datatables import Options
+from resources.datatables import FactionStatus
+from java.util import Vector
+
+
+def addTemplate(core):
+	mobileTemplate = MobileTemplate()
+	
+	mobileTemplate.setCustomName('Prefect Talmont')
+	mobileTemplate.setLevel(1)
+	mobileTemplate.setDifficulty(Difficulty.NORMAL)
+	mobileTemplate.setSocialGroup("township")
+	mobileTemplate.setOptionsBitmask(Options.INVULNERABLE)
+	mobileTemplate.setFaction("imperial")
+	mobileTemplate.setFactionStatus(FactionStatus.OnLeave)
+	  
+	templates = Vector()
+	templates.add('object/mobile/shared_prefect_talmont.iff')
+	mobileTemplate.setTemplates(templates)
+		
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/melee/unarmed/shared_unarmed_default.iff', WeaponType.UNARMED, 1.0, 6, 'kinetic')
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
+	
+	attacks = Vector()
+	mobileTemplate.setDefaultAttack('creatureMeleeAttack')
+	mobileTemplate.setAttacks(attacks)
+	
+	core.spawnService.addMobileTemplate('prefecttalmont', mobileTemplate)
+	return
