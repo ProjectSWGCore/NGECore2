eb71ee4fbae4a9452023446856a381ec1979ff2a
tacef <neuwirth.christoph@gmx.net>
2014-07-20 20:18:31 +0200

    Changed Several Spawns to use mobiletemplate
    Added several NPC's

--- scripts/static_spawns/tatooine/jawa_sandcrawler_legacy.py
+++ scripts/static_spawns/tatooine/jawa_sandcrawler_legacy.py
@@ -1,14 +1,11 @@
-import sys
 # Project SWG:   Jawa Sandcrawler - Legacy Quest Area:  Static Spawns
 # (C)2014 ProjectSWG
 
-
-
+import sys
 from resources.datatables import Options
 from resources.datatables import State
 
 def addPlanetSpawns(core, planet):
-
 
 	stcSvc = core.staticService
 	objSvc = core.objectService
@@ -16,10 +13,7 @@ def addPlanetSpawns(core, planet):
 	# TODO Check all NPCs for personalized scripting, change format.
 	
 	#Outside
-	fa2po = stcSvc.spawnObject('object/mobile/shared_3po_protocol_droid.iff', 'tatooine', long(0), float(-3805.7), float(30.4), float(-4721.6), float(0), float(0))
-	fa2po.setCustomName2('FA-2PO')	
-	fa2po.setOptionsBitmask(256)
-	
+	fa2po = stcSvc.spawnObject('fa2po', 'tatooine', long(0), float(-3805.7), float(30.4), float(-4721.6), float(0), float(0))
 	
 	#tusken point 1
 	stcSvc.spawnObject('tusken_outrider', 'tatooine', long(0), float(-3691), float(4.5), float(-4781), float(0), float(0), float(0), float(0), 45)
