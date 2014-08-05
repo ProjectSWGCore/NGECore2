fe6fc77179fc7dc14a30c9b0b0c123aeee1b94f4
tacef <neuwirth.christoph@gmx.net>
2014-08-05 20:58:16 +0200

    Added Elevatorterminals to Mustafarian bunker
    For testing Purposes only

--- scripts/object/building/player/player_house_mustafar_lg.py
+++ scripts/object/building/player/player_house_mustafar_lg.py
@@ -8,4 +8,10 @@ def setup(core, object):
 	structureterminal = core.objectService.createChildObject(object, 'object/tangible/terminal/shared_terminal_player_structure.iff', -4.1, 0.55, 6.6, 0.714 ,0.714, 1)
 	#structureterminal.setAttachment('radial_filename', 'structure_management_terminal')
 	structureterminal.setAttachment('housing_parentstruct', object)
+	
+	elevatorterminal1 = core.objectService.createChildObject(object, 'object/tangible/terminal/shared_terminal_elevator_down.iff', 0, 0.8, -0.655, -0.103, 0.998, 10)
+	
+	elevatorterminal2 = core.objectService.createChildobject(object, 'object/tangible/terminal/shared_terminal_elevator.iff', 0, -3, -0.655, 0.994, -0.103, 10)
+	
+	elevatorterminal3 = core.objectService.createChildobject(object, 'object/tangible/terminal/shared_terminal_elevator.iff', 0, -6, -0.655, 0.994, -0.103, 10)
 	return
