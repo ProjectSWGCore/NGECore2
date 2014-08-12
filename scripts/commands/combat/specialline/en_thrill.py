90db0baf57194526736c107bc15e2cc0d5f51e84
Ziggeh <ziggy@projectswg.com>
2014-08-12 10:28:21 +0200

    Changed Thrill to use a specialLine script

--- scripts/commands/combat/specialline/en_thrill.py
+++ scripts/commands/combat/specialline/en_thrill.py
@@ -4,8 +4,5 @@ def setup(core, actor, target, command):
 	core.buffService.addBuffToCreature(target, 'en_debuff_thrill', actor)
 	return
 	
-def preRun(core, actor, target, command):
-	return
-
 def run(core, actor, target, commandString):
 	return
