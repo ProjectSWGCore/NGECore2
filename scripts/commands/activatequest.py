87beb92523b91c135735a4ebfe5c84b2f795fcb1
Viaron <jyoerger10@msn.com>
2014-08-13 03:35:48 -0400

    Changed /activateQuest System Message (cuz OCD)

--- scripts/commands/activatequest.py
+++ scripts/commands/activatequest.py
@@ -5,9 +5,9 @@ def setup():
 	
 def run(core, actor, target, commandString):
 	if core.questService.adminActivateQuest(actor, commandString) is False:
-		actor.sendSystemMessage(' \\#FE2EF7 [GM] \\#FFFFFF ActivateQuest: You entered a quest that does not exist or forgot to enter one!', 0)
+		actor.sendSystemMessage(' \\#FE2EF7 [GM] \\#FFFFFF activateQuest: Error. You have specified an invalid quest.', 0)
 		return
 	
 	core.questService.sendQuestWindow(actor, commandString)
-	actor.sendSystemMessage(' \\#FE2EF7 [GM] \\#FFFFFF ActivateQuest: Command completed successfully.', 0)
+	actor.sendSystemMessage(' \\#FE2EF7 [GM] \\#FFFFFF activateQuest: Command completed successfully.', 0)
 	return
