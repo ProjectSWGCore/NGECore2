a62a56c868241071abbc71ced67d71d10ac179c7
Waverunner <waverun@live.com>
2014-08-15 18:16:45 -0400

    Changed setgodmode.py back to earlier version (accidental commit from 1st questservice commit)

--- scripts/object/tangible/quest/speeder_debris_wiring.py
+++ scripts/object/tangible/quest/speeder_debris_wiring.py
@@ -2,5 +2,4 @@ import sys
 
 def setup(core, object):
 	object.setAttachment('radial_filename', 'object/quest/speeder_debris_wiring')
-	print ('set attachment for the wiring')
 	return
--- scripts/object/tangible/quest/speeder_junk_engine.py
+++ scripts/object/tangible/quest/speeder_junk_engine.py
@@ -2,5 +2,4 @@ import sys
 
 def setup(core, object):
 	object.setAttachment('radial_filename', 'object/quest/speeder_junk_engine')
-	print ('set attachment for the speeder part')
 	return
