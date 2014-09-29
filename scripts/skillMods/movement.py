1c8fc117a43b0c52f55c234d26e925339834d18a
Zing <jeremy@zingstecharchive.com>
2014-09-28 20:34:03 -0700

    Removed modifier from movement till it works correctly

--- scripts/skillMods/movement.py
+++ scripts/skillMods/movement.py
@@ -1,10 +1,8 @@
 import sys
 
 def add(core, actor, skillMod, divisor):
-	actor.setSpeedMultiplierMod(actor.getSpeedMultiplierMod() + (float(skillMod.getBase()) / float(10)))
 	return
 
 def deduct(core, actor, skillMod, divisor):
-	actor.setSpeedMultiplierMod(actor.getSpeedMultiplierMod() - (float(skillMod.getBase()) / float(10)))
 	return
 	
