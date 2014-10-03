c2e26f40fdc96c9cfae6bac86de3d9035cbd4ffb
Zing <jeremy@zingstecharchive.com>
2014-10-02 18:32:22 -0700

    Small test for crash

--- scripts/commands/kneel.py
+++ scripts/commands/kneel.py
@@ -1,15 +1,17 @@
 import sys
+import sys
 from resources.datatables import Posture
-from tools import DevLogQueuer
 
 def setup():
 	return
 	
 def run(core, actor, target, commandString):
-	object = actor
-	print(object.getObjectID());
-	core.buffService.addBuffToCreature(actor, 'set_bonus_officer_utility_b_3', actor);
-	#core.getSWGObjectODB().put(object.getObjectID(), object);
+
+	if actor.getPosture() == Posture.Incapacitated or actor.getPosture() == Posture.Dead:
+		return
+
+	actor.setPosture(Posture.Crouched)
+	actor.setSpeedMultiplierBase(0)
+	actor.setTurnRadius(0)
 
 	return
-	
