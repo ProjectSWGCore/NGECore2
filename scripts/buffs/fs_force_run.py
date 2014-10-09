dc8ee058a0f4131fcfe4987c455359e010cc703b
Zing <jeremy@zingstecharchive.com>
2014-10-08 22:37:31 -0700

    Fixed (removedd) buff adds in fs_force_run to stop bugging character (needs command revamp to fix totally)

--- scripts/buffs/fs_force_run.py
+++ scripts/buffs/fs_force_run.py
@@ -6,10 +6,7 @@ def setup(core, actor, buff):
 
 def add(core, actor, buff):
 	actor.playEffectObject('clienteffect/pl_force_run.cef', '')
-	actor.setSpeedMultiplierBase(actor.getSpeedMultiplierBase() + 2.5 + (2.5 * (actor.getSkillModBase('expertise_movement_buff_fs_force_run')) / 100))
-	core.skillModService.addSkillMod(actor, 'slope_move', 5)
-	core.skillModService.addSkillMod(actor, 'movement_resist_snare', 100)
-	core.skillModService.addSkillMod(actor, 'movement_resist_root', 100)
+
 	
 	##For Testing Purposes
 	##actor.sendSystemMessage('You are running at ' + str(actor.getSpeedMultiplierBase()) + ' times the running speed.', 0)
@@ -18,8 +15,4 @@ def add(core, actor, buff):
 	
 def remove(core, actor, buff):
 	actor.stopEffectObject('fs_force_run')
-	actor.setSpeedMultiplierBase(actor.getSpeedMultiplierBase() - 2.5 - (2.5 * (actor.getSkillModBase('expertise_movement_buff_fs_force_run')) / 100))
-	core.skillModService.deductSkillMod(actor, 'slope_move', 5)
-	core.skillModService.deductSkillMod(actor, 'movement_resist_snare', 100)
-	core.skillModService.deductSkillMod(actor, 'movement_resist_root', 100)
 	return
