58b9f28a70c8fab81743c291443d7a817d61a8cb
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-11-09 13:08:43 -0500

    Fixed storm_commando mobile script

--- scripts/mobiles/generic/faction/imperial/storm_commando.py
+++ scripts/mobiles/generic/faction/imperial/storm_commando.py
@@ -4,31 +4,40 @@ from services.spawn import WeaponTemplate
 from resources.datatables import WeaponType
 from resources.datatables import Difficulty
 from resources.datatables import Options
+from resources.datatables import FactionStatus
 from java.util import Vector
+
 
 def addTemplate(core):
 	mobileTemplate = MobileTemplate()
 	
-	mobileTemplate.setCreatureName('storm_commando')
-	mobileTemplate.setLevel(71)
+	mobileTemplate.setCreatureName('mission_storm_commando')
+	mobileTemplate.setLevel(29)
 	mobileTemplate.setDifficulty(Difficulty.NORMAL)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(True)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setSocialGroup("imperial")
+	mobileTemplate.setAssistRange(6)
+	mobileTemplate.setStalker(True)
+	mobileTemplate.setFaction("imperial")
+	mobileTemplate.setFactionStatus(FactionStatus.Combatant)
+	
 	
 	templates = Vector()
-	templates.add('object/mobile/shared_dressed_scout_trooper_black_black.iff')
+	templates.add('object/mobile/shared_dressed_stormtrooper_commando_m.iff')
+	templates.add('object/mobile/shared_dressed_stormtrooper_commando1_m.iff')
 	mobileTemplate.setTemplates(templates)
-		
-	weaponTemplates = Vector()
-	weapontemplate = WeaponTemplate('object/weapon/ranged/pistol/shared_pistol_scout_blaster.iff', WeaponType.PISTOL, 1.0, 24, 'energy')
-	weaponTemplates.add(weapontemplate)
-	weapontemplate = WeaponTemplate('object/weapon/ranged/pistol/shared_pistol_dx2.iff', WeaponType.PISTOL, 1.0, 24, 'energy')
-	weaponTemplates.add(weapontemplate)	
-	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
 	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/ranged/rifle/shared_rifle_e11.iff', WeaponType.RIFLE, 1.0, 0, 'energy')
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
 	
 	attacks = Vector()
 	mobileTemplate.setDefaultAttack('rangedShot')
 	mobileTemplate.setAttacks(attacks)
 	
-	core.spawnService.addMobileTemplate('storm_commando', mobileTemplate)
-	
+	core.spawnService.addMobileTemplate('stormcommando', mobileTemplate)
 	return
