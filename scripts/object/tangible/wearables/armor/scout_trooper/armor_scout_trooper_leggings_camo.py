6dccc7ba444fa15cfd6bb84a7074e9bd8c5fa7a9
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-11-09 12:31:45 -0500

    Fixed faction restriction bug

--- scripts/object/tangible/wearables/armor/scout_trooper/armor_scout_trooper_leggings_camo.py
+++ scripts/object/tangible/wearables/armor/scout_trooper/armor_scout_trooper_leggings_camo.py
@@ -2,7 +2,7 @@ import sys
 
 def setup(core, object):
 	object.setIntAttribute('required_combat_level', 80)
-	object.setStringAttribute('required_faction', 'Imperial')
+	object.setStringAttribute('faction_restriction', 'Imperial')
 	object.setIntAttribute('cat_stat_mod_bonus.@stat_n:constitution_modified', 15)
 	object.setIntAttribute('cat_stat_mod_bonus.@stat_n:luck_modified', 10)
 	object.setIntAttribute('cat_stat_mod_bonus.@stat_n:precision_modified', 20)
