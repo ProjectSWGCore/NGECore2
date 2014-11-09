1b9a383f986f0ba8b6593cc8d3f5ad415d1210ea
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-11-09 13:06:29 -0500

    Fixed stormtrooper mobile script

--- scripts/mobiles/generic/faction/imperial/stormtrooper.py
+++ scripts/mobiles/generic/faction/imperial/stormtrooper.py
@@ -4,26 +4,35 @@ from services.spawn import WeaponTemplate
 from resources.datatables import WeaponType
 from resources.datatables import Difficulty
 from resources.datatables import Options
+from resources.datatables import FactionStatus
 from java.util import Vector
+
 
 def addTemplate(core):
 	mobileTemplate = MobileTemplate()
 	
-	mobileTemplate.setCreatureName('stormtrooper')
-	mobileTemplate.setLevel(90)
+	mobileTemplate.setCreatureName('mission_stormtrooper')
+	mobileTemplate.setLevel(3)
 	mobileTemplate.setDifficulty(Difficulty.NORMAL)
+	mobileTemplate.setMinSpawnDistance(4)
+	mobileTemplate.setMaxSpawnDistance(8)
+	mobileTemplate.setDeathblow(False)
+	mobileTemplate.setScale(1)
+	mobileTemplate.setSocialGroup("imperial")
+	mobileTemplate.setAssistRange(5)
+	mobileTemplate.setStalker(False)
+	mobileTemplate.setFaction("imperial")
+	mobileTemplate.setFactionStatus(FactionStatus.Combatant)
+	
 	
 	templates = Vector()
-	templates.add('object/mobile/shared_dressed_stormtrooper_m.iff')
+	templates.add('object/mobile/shared_stormtrooper.iff')
 	mobileTemplate.setTemplates(templates)
-		
-	weaponTemplates = Vector()
-	weapontemplate = WeaponTemplate('object/weapon/ranged/rifle/shared_rifle_e11.iff', WeaponType.RIFLE, 1.0, 24, 'energy')
-	weaponTemplates.add(weapontemplate)
-	weapontemplate = WeaponTemplate('object/weapon/ranged/rifle/shared_rifle_t21.iff', WeaponType.RIFLE, 1.0, 24, 'energy')
-	weaponTemplates.add(weapontemplate)	
-	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
 	
+	weaponTemplates = Vector()
+	weapontemplate = WeaponTemplate('object/weapon/ranged/rifle/shared_rifle_e11.iff', WeaponType.RIFLE, 1.0, 0, 'energy')
+	weaponTemplates.add(weapontemplate)
+	mobileTemplate.setWeaponTemplateVector(weaponTemplates)
 	
 	attacks = Vector()
 	mobileTemplate.setDefaultAttack('rangedShot')
