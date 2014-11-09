563257bf8430415692110895a4b00b2236d1fa52
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-11-09 10:12:24 -0500

    Added stats to Imperial and Rebel snow armor

--- scripts/object/tangible/wearables/armor/rebel_snow/armor_rebel_snow_boots.py
+++ scripts/object/tangible/wearables/armor/rebel_snow/armor_rebel_snow_boots.py
@@ -1,4 +1,13 @@
 import sys
 
 def setup(core, object):
-	return
+	object.setStringAttribute('bio_link', '@obj_attr_n:bio_link_pending')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_3', '@set_bonus:set_bonus_rebel_snow_armor_3')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_5', '@set_bonus:set_bonus_rebel_snow_armor_5')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_7', '@set_bonus:set_bonus_rebel_snow_armor_7')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_9', '@set_bonus:set_bonus_rebel_snow_armor_9')
+	object.setStringAttribute('@set_bonus:piece_bonus_count_11', '@set_bonus:set_bonus_rebel_snow_armor_11')
+	object.setAttachment('setBonus', 'set_bonus_rebel_snow_armor')
+	object.setStringAttribute('armor_category', '@obj_attr_n:armor_battle')
+	object.setStringAttribute('faction_restriction', 'Rebel')
+	return	
