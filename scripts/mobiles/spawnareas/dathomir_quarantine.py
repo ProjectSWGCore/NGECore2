bd33445757b4363e43bd3b1231a6ed2748aa8d73
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-11-08 19:18:31 -0500

    Added more static spawn areas for deathtroopers

--- scripts/mobiles/spawnareas/dathomir_quarantine.py
+++ scripts/mobiles/spawnareas/dathomir_quarantine.py
@@ -4,5 +4,9 @@ from java.util import Vector
 def addSpawnArea(core):
 	dynamicGroups = Vector()
 	dynamicGroups.add('dathomir_level_5_deathtroopers')
-	core.spawnService.addDynamicSpawnArea(dynamicGroups, -5700, -6478, 100, 'dathomir')
+	core.spawnService.addDynamicSpawnArea(dynamicGroups, -5700, -6500, 100, 'dathomir')
+	core.spawnService.addDynamicSpawnArea(dynamicGroups, -5640, -6550, 100, 'dathomir')
+	core.spawnService.addDynamicSpawnArea(dynamicGroups, -5850, -6400, 100, 'dathomir')
+	core.spawnService.addDynamicSpawnArea(dynamicGroups, -5800, -6500, 100, 'dathomir')
+	core.spawnService.addDynamicSpawnArea(dynamicGroups, -5600, -6525, 100, 'dathomir')
 	return
