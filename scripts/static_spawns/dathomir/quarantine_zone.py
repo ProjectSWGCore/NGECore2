cf21cd4f840507be6f38e5800590f76dedf8650f
Bronco69 <petertigges1977@googlemail.com>
2014-11-13 09:42:26 +0100

    Changed imperial guards in quarantine zone to invulnerable

--- scripts/static_spawns/dathomir/quarantine_zone.py
+++ scripts/static_spawns/dathomir/quarantine_zone.py
@@ -10,32 +10,49 @@ def addPlanetSpawns(core, planet):
 
 	# Imperial Gate Guards - Entrance + General Starting Area
 	guard1 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5775), float(510), float(-6553), float(0.36), float(0.85))
+	guard1.setOptionsBitmask(Options.INVULNERABLE)
 	guard2 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5785), float(510), float(-6545), float(0.43), float(0.85))
-	guard3 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5745), float(511), float(-6484), float(0.11), float(0.85))
-	guard3 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5744), float(511), float(-6480), float(0.37), float(0.85))
+	guard2.setOptionsBitmask(Options.INVULNERABLE)
+	guard3 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5745), float(511), float(-6484), float(0.51), float(0.85))
+	guard3.setOptionsBitmask(Options.INVULNERABLE)
+	guard4 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5744), float(511), float(-6480), float(0.51), float(0.85))
+	guard4.setOptionsBitmask(Options.INVULNERABLE)
 	liamwaurie1 = stcSvc.spawnObject('outbreak_imperial_officer_gate_keeper', 'dathomir', long(0), float(-5780), float(510), float(-6550), float(0.40), float(0.85))
 	firmuspiett1 = stcSvc.spawnObject('outbreak_imperial_officer_captain_piett', 'dathomir', long(0), float(-5749), float(510), float(-6492), float(0.40), float(0.85))
 	
 	# Imperial Gate Troopers - Exit
 	trooper1 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5793), float(510), float(-6553), float(0.56), float(0.85))
+	trooper1.setOptionsBitmask(Options.INVULNERABLE)
 	trooper2 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5784), float(510), float(-6560), float(0.28), float(0.85))
+	trooper2.setOptionsBitmask(Options.INVULNERABLE)
 	trooper3 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5789), float(510), float(-6562), float(0.23), float(0.85))
+	trooper3.setOptionsBitmask(Options.INVULNERABLE)
 	trooper4 = stcSvc.spawnObject('outbreak_imperial_stormtrooper_guards', 'dathomir', long(0), float(-5794), float(510), float(-6557), float(0.32), float(0.85))
+	trooper4.setOptionsBitmask(Options.INVULNERABLE)
 	exittrooper1 = stcSvc.spawnObject('outbreak_facility_stormtrooper_survivor', 'dathomir', long(0), float(-5783), float(510), float(-6552), float(0.38), float(0.85))
 	
 	# Camp Alpha -5915, -6645
 	alphaguard1 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-5917), float(560), float(-6640), float(0.42), float(0.85))
+	alphaguard1.setOptionsBitmask(Options.INVULNERABLE)
 	alphaguard2 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-5908), float(560), float(-6645), float(0.34), float(0.85))
+	alphaguard2.setOptionsBitmask(Options.INVULNERABLE)
 	alphaguard3 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-5918), float(560), float(-6650), float(0.60), float(0.85))
+	alphaguard3.setOptionsBitmask(Options.INVULNERABLE)
 	alphaguard4 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-5913), float(560), float(-6670), float(-0.63), float(0.85))
-	alphaguard3 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-5930), float(560), float(-6690), float(0.26), float(0.85))
+	alphaguard4.setOptionsBitmask(Options.INVULNERABLE)
+	alphaguard5 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-5930), float(560), float(-6690), float(0.26), float(0.85))
+	alphaguard5.setOptionsBitmask(Options.INVULNERABLE)
 	#alphasurvivor1 = stcSvc.spawnObject('outbreak_camp_alpha_survivor', 'dathomir', long(0), float(-5930), float(560), float(-6690), float(0.26), float(0.85))
 	
 	# Camp Beta -6290 -7529
 	betaguard1 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-6300), float(560), float(-7535), float(-0.86), float(0.85))
+	betaguard1.setOptionsBitmask(Options.INVULNERABLE)
 	betaguard2 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-6300), float(560), float(-7528), float(-0.88), float(0.85))
+	betaguard2.setOptionsBitmask(Options.INVULNERABLE)
 	betaguard3 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-6290), float(560), float(-7520), float(1.38), float(0.85))
+	betaguard3.setOptionsBitmask(Options.INVULNERABLE)
 	betaguard4 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-6280), float(560), float(-7520), float(-1.04), float(0.85))
+	betaguard4.setOptionsBitmask(Options.INVULNERABLE)
 	#beltasurvivor1 = stcSvc.spawnObject('outbreak_camp_beta_survivor', 'dathomir', long(0), float(-5930), float(560), float(-6690), float(0.26), float(0.85))
 	
 	# Camp Gamma -6815 -6454
@@ -43,10 +60,15 @@ def addPlanetSpawns(core, planet):
 	
 	# Camp Delta -7142 -6941
 	deltaguard1 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-7155), float(560), float(-6955), float(-1.29), float(0.85))
+	deltaguard1.setOptionsBitmask(Options.INVULNERABLE)
 	deltaguard2 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-7135), float(560), float(-6955), float(1.10), float(0.85))
+	deltaguard2.setOptionsBitmask(Options.INVULNERABLE)
 	deltaguard3 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-7150), float(560), float(-6940), float(1.20), float(0.85))
+	deltaguard3.setOptionsBitmask(Options.INVULNERABLE)
 	deltaguard4 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-7143), float(560), float(-6910), float(1.78), float(0.85))
+	deltaguard4.setOptionsBitmask(Options.INVULNERABLE)
 	deltaguard5 = stcSvc.spawnObject('outbreak_mixed_guard_aggro', 'dathomir', long(0), float(-7150), float(560), float(-6910), float(1.75), float(0.85))
+	deltaguard5.setOptionsBitmask(Options.INVULNERABLE)
 	#beltasurvivor1 = stcSvc.spawnObject('outbreak_camp_delta_survivor', 'dathomir', long(0), float(-5930), float(560), float(-6690), float(0.26), float(0.85))
 	
 	# Camp Epsilon -7463 -7392
